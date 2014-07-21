c90c4db50796e3708499c93c871d1ab8968f092a
Saku Ytti <saku@ytti.fi>
2014-07-21 16:17:09 +0300

    Fix bug in 'vars.rb'
    We need to return value of 'r' instead of value of previous evaluation.

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+# 0.1.1
+- BUGFIX: vars needs to return value of r, not value of evaluation
--- lib/oxidized/config/vars.rb
+++ lib/oxidized/config/vars.rb
@@ -5,5 +5,6 @@ module Oxidized::Config::Vars
     r =   @node.vars[name] unless @node.vars.nil?
     r ||= Oxidized::CFG.groups[@node.group].vars[name.to_s] if Oxidized::CFG.groups.has_key?(@node.group)
     r ||= Oxidized::CFG.vars[name.to_s] if Oxidized::CFG.vars.has_key?(name)
+    r
   end
 end
