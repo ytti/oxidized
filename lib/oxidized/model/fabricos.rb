981323ff57593567eed4032652d16ada43d69e77
Tomi Hakala <tomi.hakala@itvirtuoosit.fi>
2014-07-21 20:24:56 +0300

    Added Brocade Fabric OS support

---
+++ lib/oxidized/model/fabricos.rb
@@ -0,0 +1,29 @@
+class FabricOS < Oxidized::Model
+
+  # Brocade Fabric OS model #
+  ## FIXME: Only ssh exec mode support, no telnet, no ssh screenscraping
+
+  prompt /^([\w]+:+[\w]+[>]\s)$/
+  comment  '# '
+
+  cmd :all do |cfg|
+    cfg
+  end
+
+  cmd 'chassisShow' do |cfg|
+    comment cfg
+  end
+
+  cmd 'configShow -all' do |cfg|
+    cfg
+  end
+
+  cfg :ssh do
+    exec true  # don't run shell, run each command in exec channel
+  end
+
+  cfg :ssh do
+    pre_logout 'exit'
+  end
+
+end
