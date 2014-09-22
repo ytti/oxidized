4a57251c79719a1bedbfe73d8cb5171ff48285cd
Eric <eric@emj.se>
2014-09-22 22:08:53 +0200

    Added model for cisco NXOS

---
+++ lib/oxidized/model/nxos.rb
@@ -0,0 +1,23 @@
+class NXOS < Oxidized::Model
+
+  prompt /^(\r?[\w.@_()-]+[#]\s?)$/
+  comment '! '
+
+  cmd 'show version' do |cfg|
+    cfg = cfg.each_line.take_while { |line| not line.match(/uptime/i) }
+    comment cfg.join ""
+  end
+
+  cmd 'show inventory' do |cfg|
+    comment cfg
+  end 
+
+  cmd 'show running-config' do |cfg|
+    cfg.gsub! /^!Time:[^\n]*\n/, ''
+  end
+
+  cfg :ssh do
+    post_login 'terminal length 0'
+    pre_logout 'exit'
+  end
+end
