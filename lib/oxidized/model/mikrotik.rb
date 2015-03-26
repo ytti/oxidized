804dcbc251f576cf12bce6ffc6759c48dbd87662
Saku Ytti <saku@ytti.fi>
2015-03-26 23:54:55 +0200

    remove whitespace, rename class

--- lib/oxidized/model/mikrotik.rb
+++ lib/oxidized/model/mikrotik.rb
@@ -1,17 +1,17 @@
-class Mikrotik < Oxidized::Model
+class RouterOS < Oxidized::Model
   prompt /^\[\w+@\S+\]\s?>\s?$/
   comment "# "
 
-  cmd '/system routerboard print' do |cfg|                                                               
-    comment cfg                                                                                          
-  end                                                                                                    
+  cmd '/system routerboard print' do |cfg|
+    comment cfg
+  end
 
-  cmd '/export' do |cfg|                                                                                 
-    cfg = cfg.split("\n").select { |line| not line[/^\#\s\w{3}\/\d{2}\/\d{4}.*$/] }                      
-    cfg.join("\n") + "\n"                                                                                
-  end                                                                                                    
+  cmd '/export' do |cfg|
+    cfg = cfg.split("\n").select { |line| not line[/^\#\s\w{3}\/\d{2}\/\d{4}.*$/] }
+    cfg.join("\n") + "\n"
+  end
 
-  cfg :ssh do                                                                                            
-    exec true                                                                                            
-  end                                                                                                    
-end                 
+  cfg :ssh do
+    exec true
+  end
+end
