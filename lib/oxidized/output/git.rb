215cc4074083a65ebd9d3189532073882afd75e3
jwhited <jwhited0917@gmail.com>
2015-07-05 16:17:39 -0700

    fix fetching when groups are used and single_repo: true

--- lib/oxidized/output/git.rb
+++ lib/oxidized/output/git.rb
@@ -50,13 +50,17 @@ class Git < Output
   def fetch node, group
     begin
       repo = @cfg.repo
-      if group
+      if (group && !@cfg.single_repo?)
         repo = File.join File.dirname(repo), group + '.git'
       end
       repo = Rugged::Repository.new repo
       index = repo.index
       index.read_tree repo.head.target.tree unless repo.empty?
-      repo.read(index.get(node)[:oid]).data
+      if (group && @cfg.single_repo?)
+        repo.read(index.get(group + '/' + node)[:oid]).data
+      else
+        repo.read(index.get(node)[:oid]).data
+      end
     rescue
       'node not found'
     end
