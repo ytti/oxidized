5212f8508d3cc561be1187199b98c6c523c35335
root <florian.doublet@hotmail.fr>
2015-07-21 14:51:51 +0200

    Stores stats in sqlite3. Need the gem sqlite3

--- lib/oxidized/job.rb
+++ lib/oxidized/job.rb
@@ -1,14 +1,21 @@
 module Oxidized
+  require 'oxidized/store'
   class Job < Thread
     attr_reader :start, :end, :status, :time, :node, :config
     def initialize node
       @node         = node
-      @start        = Time.now.utc
+      
+      store = Store.new
+      #store stats in sql database and return a hashmap
+      hash = store.update_stats node
+      
+      @start        = hash[:start]
       super do |node|
-        @status, @config = node.run
-        @end             = Time.now.utc
-        @time            = @end - @start
-      end
+      @status       = hash[:status]
+      @config       = hash[:config]
+      @end          = hash[:end]
+      @time         = hash[:time]
+      end  
     end
   end
 end
