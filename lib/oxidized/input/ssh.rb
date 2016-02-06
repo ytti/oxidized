3e28af7dcbfafbd5cf1f7e2cbfae984176113a3b
nopedial <sam@arahant.net>
2016-02-06 18:28:22 +0000

    set TERM

--- lib/oxidized/input/ssh.rb
+++ lib/oxidized/input/ssh.rb
@@ -86,7 +86,7 @@ module Oxidized
           @output << data
           @output = @node.model.expects @output
         end
-        ch.request_pty do |_ch, success_pty|
+        ch.request_pty (opts={:term=>'vt100'}) do |_ch, success_pty|
           raise NoShell, "Can't get PTY" unless success_pty
           ch.send_channel_request 'shell' do |_ch, success_shell|
             raise NoShell, "Can't get shell" unless success_shell
