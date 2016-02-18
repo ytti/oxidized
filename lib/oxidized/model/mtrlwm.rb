59711f9b83dae449aac4e4cb058c3f1e7d69eb0c
roedie <github@roedie.nl>
2016-02-18 12:35:53 +0100

    Add support for Motorola wireless controllers

---
+++ lib/oxidized/model/mtrlwm.rb
@@ -0,0 +1,35 @@
+class mtrlwm < Oxidized::Model
+
+  # Motorola WM3400
+
+  comment  '# '
+
+  cmd :all do |cfg|
+    # xos inserts leading \r characters and other trailing white space.
+    # this deletes extraneous \r and trailing white space.
+    cfg.each_line.to_a[1..-2].map{|line|line.delete("\r").rstrip}.join("\n") + "\n"
+  end
+
+  cmd 'show version' do |cfg|
+    comment cfg
+  end
+
+  cmd 'show licenses' do |cfg|
+    comment cfg
+  end
+
+  cmd 'show running-config'
+
+  cfg :telnet do
+    username /^login:/
+    password /^\r*password:/
+  end
+
+  cfg :telnet, :ssh do
+    post_login 'terminal length 0'
+    pre_logout 'exit'
+    pre_logout 'n'
+  end
+
+end
+
