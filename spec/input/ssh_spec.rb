1a68e83169f1fc16d8a222cb18f3f390c9c5d51d
roedie <github@roedie.nl>
2016-02-25 09:14:20 +0100

    Update spec: Change proxy to ssh_proxy

--- spec/input/ssh_spec.rb
+++ spec/input/ssh_spec.rb
@@ -12,7 +12,7 @@ describe Oxidized::SSH do
                                model: 'junos',
                                username: 'alma',
                                password: 'armud',
-                               vars: {proxy: 'test.com'})
+                               vars: {ssh_proxy: 'test.com'})
 
   end
 
@@ -25,10 +25,10 @@ describe Oxidized::SSH do
       @node.expects(:model).returns(model)
 
       proxy = mock()
-      Net::SSH::Proxy::Command.expects(:new).with("ssh test.com nc %h %p").returns(proxy)
+      Net::SSH::Proxy::Command.expects(:new).with("ssh test.com -W %h:%p").returns(proxy)
       Net::SSH.expects(:start).with('93.184.216.34', 'alma', {:port => 22, :password => 'armud', :timeout => Oxidized.config.timeout,
                                     :paranoid => Oxidized.config.input.ssh.secure, :auth_methods => ['none', 'publickey', 'password', 'keyboard-interactive'],
-                                    :number_of_password_prompts => 0, :proxy => proxy})
+                                    :number_of_password_prompts => 0, :ssh_roxy => proxy})
 
       ssh.instance_variable_set("@exec", true)
       ssh.connect(@node)
