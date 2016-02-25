82da30aa38329208126c37829501956162fa2c4d
Saku Ytti <saku@ytti.fi>
2016-02-25 11:27:30 +0200

    fix ssh test
    also fix warning about unused variable

--- spec/input/ssh_spec.rb
+++ spec/input/ssh_spec.rb
@@ -28,7 +28,7 @@ describe Oxidized::SSH do
       Net::SSH::Proxy::Command.expects(:new).with("ssh test.com -W %h:%p").returns(proxy)
       Net::SSH.expects(:start).with('93.184.216.34', 'alma', {:port => 22, :password => 'armud', :timeout => Oxidized.config.timeout,
                                     :paranoid => Oxidized.config.input.ssh.secure, :auth_methods => ['none', 'publickey', 'password', 'keyboard-interactive'],
-                                    :number_of_password_prompts => 0, :ssh_roxy => proxy})
+                                    :number_of_password_prompts => 0, :proxy => proxy})
 
       ssh.instance_variable_set("@exec", true)
       ssh.connect(@node)
