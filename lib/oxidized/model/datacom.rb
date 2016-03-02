da404dd3c02a35591b17617875f7cb0d6ea47f3a
Danilo Sousa <dsgoncalves@uoldiveo.com>
2016-03-02 18:05:41 -0300

    add DataCom support

---
+++ lib/oxidized/model/datacom.rb
@@ -0,0 +1,33 @@
+class DataCom < Oxidized::Model
+
+  comment '! '
+
+  expect /^--More--\s+$/ do |data, re|
+    send ' '
+    data.sub re, ''
+  end
+
+  cmd :all do |cfg|
+    cfg.each_line.to_a[1..-2].join
+    cfg.cut_head.cut_tail
+  end
+
+  cmd 'show firmware' do |cfg|
+    comment cfg
+  end
+
+  cmd 'show system' do |cfg|
+    comment cfg
+  end
+
+  cmd 'show running-config' do |cfg|
+    cfg.cut_head
+  end
+
+  cfg :telnet do
+    username /login:\s$/
+    password /^Password:\s$/
+    pre_logout 'exit'
+  end
+
+end
