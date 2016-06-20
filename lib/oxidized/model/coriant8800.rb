0f4803f627d3fef797d34a8afc43555c54c28e69
Everton Marques <everton.marques@gmail.com>
2016-06-20 16:09:13 -0300

    Coriant8800 from https://www.coriant.com/products/8800.asp

---
+++ lib/oxidized/model/coriant8800.rb
@@ -0,0 +1,20 @@
+class Coriant8800 < Oxidized::Model
+
+  prompt /^[^\s#]+#\s/
+
+  cmd 'show node extensive' do |cfg|
+    comment cfg
+  end
+
+  cmd 'show run' do |cfg|
+    cfg
+  end
+
+  cfg :telnet, :ssh do
+    username /^Login:\s$/
+    password /^Password:\s$/
+    pre_logout 'exit'
+    post_login 'enable config terminal length 0'
+  end
+
+end
