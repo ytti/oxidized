e5dc1dedf976a12c278f68c284c976ed462cdcb4
Saku Ytti <saku@ytti.fi>
2016-11-01 02:04:31 +0200

    expect prompt after sending enable password

--- lib/oxidized/model/dnos.rb
+++ lib/oxidized/model/dnos.rb
@@ -36,7 +36,7 @@ class DNOS  < Oxidized::Model
     if vars :enable
       post_login do
         send "enable\n"
-        send vars(:enable) + "\n"
+        cmd vars(:enable)
       end
     end
     post_login 'terminal length 0'
