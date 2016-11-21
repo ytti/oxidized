2bc27ee28747516d172101a1b8ae73aa44c7e23b
Clay Curtis <clay584@gmail.com>
2016-11-21 11:57:44 -0500

    removed 'Building configuration...' output from configs

--- lib/oxidized/model/iosxr.rb
+++ lib/oxidized/model/iosxr.rb
@@ -24,10 +24,12 @@ class IOSXR < Oxidized::Model
   end
 
   cmd 'admin show running-config' do |cfg|
+    cfg = cfg.each_line.to_a[1..-1].join
     cfg
   end
 
   cmd 'show running-config' do |cfg|
+    cfg = cfg.each_line.to_a[1..-1].join
     cfg
   end
 
