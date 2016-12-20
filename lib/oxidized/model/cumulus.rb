7c323a2e5d19f69fae8de69b9d91bdea01592ed1
nertwork <webmaster@nertwork.com>
2016-12-20 11:01:56 -0800

    Quagga fix

--- lib/oxidized/model/cumulus.rb
+++ lib/oxidized/model/cumulus.rb
@@ -49,7 +49,7 @@ class Cumulus < Oxidized::Model
     cfg += cmd 'cat /etc/quagga/ospf6d.conf'
     
     cfg += add_comment 'QUAGGA CONF'
-    cfg += cmd 'cat /etc/quagga/Quagga.conf
+    cfg += cmd 'cat /etc/quagga/Quagga.conf'
     
     cfg += add_comment 'MOTD'
     cfg += cmd 'cat /etc/motd'
