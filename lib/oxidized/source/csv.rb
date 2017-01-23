737ffa54eb42537f9fbd6bb6a2a708507fd802e3
mbakerbp <michael.baker@bulletproof.net>
2017-01-24 07:40:36 +0800

    Update

--- lib/oxidized/source/csv.rb
+++ lib/oxidized/source/csv.rb
@@ -19,6 +19,7 @@ class CSV < Source
 
   def load
     nodes = []
+    crypto = GPGME::Crypto.new
     file = (@cfg.gpg == 'false') ? open(File.expand_path @cfg.file) : crypto.decrypt(File.open(@cfg.file))
     open(file).each_line do |line|
       next if line.match(/^\s*#/)
