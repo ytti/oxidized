a1e6277280b1882757f369cb57358c5cbc6aa170
Tim Pray <tim.pray@displays2go.com>
2017-02-07 12:22:35 -0500

    Better notes and fix a mistake in the secret lines

--- lib/oxidized/model/aosw.rb
+++ lib/oxidized/model/aosw.rb
@@ -1,8 +1,14 @@
 class AOSW < Oxidized::Model
 
-  # AOSW Aruba Wireless
+  # AOSW Aruba Wireless, IAP, Instant Controller and Mobility Access Switches
   # Used in Alcatel OAW-4750 WLAN controller
   # Also Dell controllers
+  
+  # HPE Aruba Switches should use a different model as the software is based on the HP Procurve line.
+  
+  # Support for IAP & Instant Controller tested with 115, 205, 215 & 325 running 6.4.4.8-4.2.4.5_57965
+  # Support for Mobility Access Switches tested with S2500-48P & S2500-24P running 7.4.1.4_54199 and S2500-24P running 7.4.1.7_57823
+  # All IAPs connected to a Instant Controller will have the same config output. Only the controller needs to be monitored. 
 
   comment  '# '
   prompt /^\(?.+\)?\s?[#>]/
@@ -12,6 +18,7 @@ class AOSW < Oxidized::Model
   end
 
   cmd :secret do |cfg|
+    cfg.gsub!(/secret (\S+)$/, 'secret <secret removed>')
     cfg.gsub!(/enable secret (\S+)$/, 'enable secret <secret removed>')
     cfg.gsub!(/PRE-SHARE (\S+)$/, 'PRE-SHARE <secret removed>')
     cfg.gsub!(/ipsec (\S+)$/, 'ipsec <secret removed>')
@@ -21,8 +28,8 @@ class AOSW < Oxidized::Model
     cfg.gsub!(/mobility-manager (\S+) user (\S+) (\S+)/, 'mobility-manager \1 user \2 <secret removed>')
     cfg.gsub!(/mgmt-user (\S+) (root|guest\-provisioning|network\-operations|read\-only|location\-api\-mgmt) (\S+)$/, 'mgmt-user \1 \2 <secret removed>') #MAS & Wireless Controler 
     cfg.gsub!(/mgmt-user (\S+) (\S+)( (read\-only|guest\-mgmt))?$/, 'mgmt-user \1 <secret removed> \3') #IAP 
-#MAS format: mgmt-user <username> <accesslevel>   <password hash>
-#IAP format: mgmt-user <username> <password hash>
+#MAS format: mgmt-user <username> <accesslevel> <password hash>
+#IAP format (root user): mgmt-user <username> <password hash>
 #IAP format: mgmt-user <username> <password hash> <access level>
     cfg.gsub!(/key (\S+)$/, 'key <secret removed>')
     cfg.gsub!(/wpa-passphrase (\S+)$/, 'wpa-passphrase <secret removed>')
@@ -38,17 +45,17 @@ class AOSW < Oxidized::Model
   end
 
   cmd 'show inventory' do |cfg|
-    cfg = "" if cfg.match /(Invalid input detected at '\^' marker|Parse error)/ #Don't show for unsupported devices
+    cfg = "" if cfg.match /(Invalid input detected at '\^' marker|Parse error)/ #Don't show for unsupported devices (IAP and MAS)
     rstrip_cfg clean cfg
   end
 
   cmd 'show slots' do |cfg|
-    cfg = "" if cfg.match /(Invalid input detected at '\^' marker|Parse error)/ #Don't show for unsupported devices
+    cfg = "" if cfg.match /(Invalid input detected at '\^' marker|Parse error)/ #Don't show for unsupported devices (IAP and MAS)
     rstrip_cfg comment cfg
   end
 
   cmd 'show license' do |cfg|
-    cfg = "" if cfg.match /(Invalid input detected at '\^' marker|Parse error)/ #Don't show for unsupported devices
+    cfg = "" if cfg.match /(Invalid input detected at '\^' marker|Parse error)/ #Don't show for unsupported devices (IAP and MAS)
     rstrip_cfg comment cfg
   end
 
