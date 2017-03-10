88349a03aa32a13a6cc427f635f717dae2c77509
Nat Morris <nat@nuqe.net>
2017-03-10 18:35:30 +0000

    continue to diffs by default, but optionally post formatted messages

--- lib/oxidized/hook/slackdiff.rb
+++ lib/oxidized/hook/slackdiff.rb
@@ -1,5 +1,8 @@
 require 'slack'
 
+# defaults to posting a diff, if messageformat is supplied them a message will be posted too
+# diffenable defaults to true
+
 class SlackDiff < Oxidized::Hook
   def validate_cfg!
     raise KeyError, 'hook.token is required' unless cfg.has_key?('token')
@@ -13,16 +16,33 @@ class SlackDiff < Oxidized::Hook
         client = Slack::Client.new token: cfg.token
         client.auth_test
         log "Connected"
-        gitoutput = ctx.node.output.new
-        diff = gitoutput.get_diff ctx.node, ctx.node.group, ctx.commitref, nil
-        title = "#{ctx.node.name.to_s} #{ctx.node.group.to_s} #{ctx.node.model.class.name.to_s.downcase}"
-        log "Posting diff as snippet to #{cfg.channel}"
-        client.files_upload(channels: cfg.channel, as_user: true,
-                             content: diff[:patch].lines.to_a[4..-1].join,
-                             filetype: "diff",
-                             title: title,
-                             filename: "change"
-                            )
+        # diff snippet - default
+        diffenable = true
+        if cfg.has_key?('diff') == true
+          if cfg.diff == false
+            diffenable = false
+          end
+        end
+        if diffenable == true
+          gitoutput = ctx.node.output.new
+          diff = gitoutput.get_diff ctx.node, ctx.node.group, ctx.commitref, nil
+          title = "#{ctx.node.name.to_s} #{ctx.node.group.to_s} #{ctx.node.model.class.name.to_s.downcase}"
+          log "Posting diff as snippet to #{cfg.channel}"
+          client.files_upload(channels: cfg.channel, as_user: true,
+                              content: diff[:patch].lines.to_a[4..-1].join,
+                              filetype: "diff",
+                              title: title,
+                              filename: "change"
+                             )
+        end
+        # message custom formatted - optional
+        if cfg.has_key?('message') == true
+          log cfg.message
+          msg = cfg.message % {:node => ctx.node.name.to_s, :group => ctx.node.group.to_s, :commitref => ctx.commitref, :model => ctx.node.model.class.name.to_s.downcase}
+          log msg
+          log "Posting message to #{cfg.channel}"
+          client.chat_postMessage(channel: "#oxidized-test", text: msg,  as_user: true)
+        end
         log "Finished"
       end
     end
