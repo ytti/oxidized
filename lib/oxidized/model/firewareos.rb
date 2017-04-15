76e01672790843de33df67dd76686c00460377d8
Mike Nowak <mikenowak@users.noreply.github.com>
2017-04-15 18:46:09 +0900

    Support for Watchguard Logon Disclaimer

--- lib/oxidized/model/firewareos.rb
+++ lib/oxidized/model/firewareos.rb
@@ -7,6 +7,12 @@ class FirewareOS < Oxidized::Model
     cfg.each_line.to_a[1..-2].join
   end
 
+  # Handle Logon Disclaimer added in XTM 11.9.3
+  expect /^I have read and accept the Logon Disclaimer message. \(yes or no\)\? $/ do |data, re|
+    send "yes\n"
+    data.sub re, ''
+  end
+
   cmd 'show sysinfo' do |cfg|
     # avoid commits due to uptime
     cfg = cfg.each_line.select { |line| not line.match /(.*time.*)|(.*memory.*)|(.*cpu.*)/ }
