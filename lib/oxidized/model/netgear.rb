1f2a11069f93a2997dac387e90010eacc20e2375
Alexander Schaber <alexander@schaber.info>
2017-04-27 09:18:00 +0200

    include solution for paging on netgear

--- lib/oxidized/model/netgear.rb
+++ lib/oxidized/model/netgear.rb
@@ -3,28 +3,24 @@ class Netgear < Oxidized::Model
   comment '!'
   prompt /^(\([\w-]+\)\s[#>])$/
 
-  expect /^--More-- or \(q\)uit/ do |data, re|
-    send ' '
-    data.sub re, ''
-  end
-
   cmd :secret do |cfg|
     cfg.gsub!(/password (\S+)/, 'password <hidden>')
     cfg
   end
 
-  cfg :ssh do
+  cfg :telnet, :ssh do
     if vars :enable
       post_login do
-        send "enable\n"
+        cmd 'enable'
         # Interpret enable: true as meaning we won't be prompted for a password
         unless vars(:enable).is_a? TrueClass
           expect /[pP]assword:\s?$/
-          send vars(:enable) + "\n"
+          cmd vars(:enable) + "\n"
         end
         expect /^.+[#]$/
       end
     end
+    post_login 'terminal length 0'
     pre_logout 'exit'
     pre_logout 'quit'
   end
@@ -34,4 +30,3 @@ class Netgear < Oxidized::Model
   end
 
 end
-
