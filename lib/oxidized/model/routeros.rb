89aa488c7bd6bf90adc17824fd42850390bdb0c5
Richard Mayhew <richard@splash.co.za>
2017-05-14 16:17:05 +0200

    Included the changelog for RouterOS

--- lib/oxidized/model/routeros.rb
+++ lib/oxidized/model/routeros.rb
@@ -10,6 +10,10 @@ class RouterOS < Oxidized::Model
     comment cfg
   end
 
+  cmd '/system history print' do |cfg|
+    comment cfg
+  end
+
   cmd '/export' do |cfg|
     cfg.gsub! /\x1B\[([0-9]{1,3}((;[0-9]{1,3})*)?)?[m|K]/, '' # strip ANSI colours
     cfg.gsub! /\\\r\n\s+/, ''   # strip new line
