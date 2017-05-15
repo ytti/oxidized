97133ec863c695c6eaea6a4da523128234344295
ospfbgp <danielcoxman@gmail.com>
2017-05-15 00:44:31 -0400

    Update voss.rb because of git diff
    every config update was making a diff because temperature was included in "show sys-info".  Used more specific commands so temperature is not included now.

--- lib/oxidized/model/voss.rb
+++ lib/oxidized/model/voss.rb
@@ -1,22 +1,25 @@
 class Voss < Oxidized::Model
   # Avaya VSP Operating System Software(VOSS)
   # Created by danielcoxman@gmail.com
-  # May 9, 2017
+  # May 15, 2017
   # This was tested on vsp4k and vsp8k
 
   comment '# '
 
   prompt /^[^\s#>]+[#>]$/
 
-  cmd 'show sys-info' do |cfg|
-    comment cfg
-  end
+  # needed for proper formatting after post_login
+  cmd('') { |cfg| comment "#{cfg}\n" }
+  # get some general information about switch
+  cmd('show sys-info card') { |cfg| comment "#{cfg}\n" }
+  cmd('show sys-info fan') { |cfg| comment "#{cfg}\n" }
+  cmd('show sys-info power') { |cfg| comment "#{cfg}\n" }
 
   # more the config rather than doing a show run
   cmd 'more config.cfg' do |cfg|
     cfg
     cfg.gsub! /^[^\s#>]+[#>]$/, ''
-    cfg.gsub! /^more config.cfg/, ''
+    cfg.gsub! /^more config.cfg/, '# more config.cfg'
   end
 
   cfg :telnet do
@@ -24,10 +27,14 @@ class Voss < Oxidized::Model
     password /Password: $/
   end
 
-  cfg :telnet, :ssh do
+cfg :telnet, :ssh do
     pre_logout 'exit'
-    post_login 'enable'
-    post_login 'terminal more disable'
+    post_login do
+      send "enable\n"
+      send "terminal more disable\n"
+      # Backup the config via tftp to a tftpserver of your choice
+      #send "copy config.cfg x.x.x.x:" + node.name + ".cfg\n"
+    end
   end
 
 end
