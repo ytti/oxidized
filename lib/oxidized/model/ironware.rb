2455a32780ceadd68d356b194275279046052ae1
Paul Beers <paul@peezs.nl>
2017-06-08 16:45:33 +0200

    ironware.rb changed to Fix ADX's reporting fan speeds

--- lib/oxidized/model/ironware.rb
+++ lib/oxidized/model/ironware.rb
@@ -34,6 +34,7 @@ class IronWare < Oxidized::Model
     cfg.gsub! /(^((.*)Current temp(.*))$)/, '' #remove unwanted lines current temperature
     cfg.gsub! /Speed = [A-Z-]{2,6} \(\d{2,3}\%\)/, '' #remove unwanted lines Speed Fans
     cfg.gsub! /current speed is [A-Z]{2,6} \(\d{2,3}\%\)/, ''
+    cfg.gsub! /Fan [1-6] - STATUS: OK [A-Z()[0-9] :-]{0,30}/, '' # Fix for ADX Fan speed reporting
     cfg.gsub! /([\[]*)1([\]]*)<->([\[]*)2([\]]*)(<->([\[]*)3([\]]*))*/, ''
     cfg.gsub! /\d{2}\.\d deg-C/, 'XX.X deg-C'
     if cfg.include? "TEMPERATURE"
