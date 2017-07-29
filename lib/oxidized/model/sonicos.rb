682fb121af23433e8403f31155d89c77ff724328
Dutt, Ron <ron.dutt@janicki.com>
2017-07-28 17:39:12 -0700

    added secrets handling

--- lib/oxidized/model/sonicos.rb
+++ lib/oxidized/model/sonicos.rb
@@ -3,34 +3,31 @@ class SonicOS < Oxidized::Model
   prompt /^\w+@\w+[>]\(?.+\)?\s?/
   comment  '! '
 
-  expect /\-\-MORE\-\-/ do |data, re|
-    send ' '
-    data.sub re, ''
-  end
-
   cmd :all do |cfg|
-    cfg.gsub! /^% Invalid input detected at '\^' marker\.$|^\s+\^$/, ''
     cfg.each_line.to_a[1..-2].join
   end
 
   cmd :secret do |cfg|
+    cfg.gsub! /cli ftp password default \d\,(\S+)/, 'cli ftp password default <secret hidden> \2'
+    cfg.gsub! /secret \d\,(\S+)/, 'secret <secret hidden> \2'
+    cfg.gsub! /shared-secret \d\,(\S+)/, 'shared-secret <secret hidden> \2'
     cfg
   end
 
   cmd 'show version' do |cfg|
-    cfg = cfg.each_line.select { |line| not line.match /system-time/ }
-    cfg = cfg.each_line.select { |line| not line.match /(\s+up\s+\d+\s+)|(.*Days.*)/ }
-    cfg = cfg.join
+    comment cfg.each_line.reject { |line| line.match /system-time \"\d{2}\/\d{2}\/\d{4} \d{2}\:\d{2}:\d{2}.\d+\"/ or line.match /system-uptime "(\s+up\s+\d+\s+)|(.*Days.*)"/ }.join
     comment cfg
   end
 
   cmd 'show current-config' do |cfg|
+    cfg.each_line.reject { |line| line.match /checksum \d+/ }.join
     cfg = cfg.each_line.to_a[3..-1].join
     cfg.gsub! /^: [^\n]*\n/, ''
     cfg
   end
 
   cfg :ssh do
+    post_login 'no cli pager session'
     pre_logout 'exit'
   end
 
