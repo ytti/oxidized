903cbd16e53b0339aac7a1192b3bd129ce552565
Koen van den Heuvel <hello@koenvdheuvel.me>
2017-08-01 12:37:29 +0200

    Update enterasys.rb remove empty and comment lines

--- lib/oxidized/model/enterasys.rb
+++ lib/oxidized/model/enterasys.rb
@@ -1,20 +1,25 @@
 class Enterasys < Oxidized::Model
 
-  # Enterasys B3 model #
+  # Enterasys B3/C3 models #
 
   prompt /^.+\w\(su\)->\s?$/
 
   comment  '!'
 
   cmd :all do |cfg|
-     cfg.each_line.to_a[1..-2].join
+     cfg.each_line.to_a[2..-3].map{|line|line.delete("\r").rstrip}.join("\n") + "\n"
   end
 
   cmd 'show system hardware' do |cfg|
-    cfg
+    comment cfg
   end
 
   cmd 'show config' do |cfg|
+    cfg.gsub! /^This command shows non-default configurations only./, ''
+    cfg.gsub! /^Use 'show config all' to show both default and non-default configurations./, ''
+    cfg.gsub! /^!|#.*/, ''
+    cfg.gsub! /^$\n/, ''
+
     cfg
   end
 
