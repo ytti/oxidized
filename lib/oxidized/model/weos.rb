cd41bb4cd4c031e951854bfc46ad77fd934a6789
ignaqui <ignaqui@users.noreply.github.com>
2017-08-01 09:04:01 -0400

    Driver for Westell 8178G and Westell 8266G devices

---
+++ lib/oxidized/model/weos.rb
@@ -0,0 +1,22 @@
+class WEOS < Oxidized::Model
+
+  #Westell WEOS, works with Westell 8178G, Westell 8266G
+
+  prompt /^(\s[\w.@-]+[#>]\s?)$/
+
+  cmd :all do |cfg|
+    cfg.each_line.to_a[1..-2].join
+  end
+
+  cmd 'show running-config' do |cfg|
+    cfg
+  end
+
+  cfg :telnet do
+    username /login:/
+    password /assword:/
+    post_login 'cli more disable'
+    pre_logout 'logout'
+  end
+
+end
