b13c911751f18165d681610c29f43cb61d631a03
Nagainos <root@nagainos.net>
2017-08-15 14:20:57 +0200

    Added enable mode support for Ericsson SE100

--- lib/oxidized/model/ipos.rb
+++ lib/oxidized/model/ipos.rb
@@ -3,7 +3,7 @@ class IPOS < Oxidized::Model
   # Ericsson SSR (IPOS)
   # Redback SE (SEOS)
 
-  prompt /^([\[\]\w.@-]+[#>]\s?)$/
+  prompt /^([\[\]\w.@-]+[#:>]\s?)$/
   comment '! '
 
   cmd 'show chassis' do |cfg|
@@ -52,6 +52,12 @@ class IPOS < Oxidized::Model
 
   cfg :telnet, :ssh do
     post_login 'terminal length 0'
+    if vars :enable
+      post_login do
+        cmd "enable" 
+        cmd vars(:enable)
+      end
+    end
     pre_logout do
       send "exit\n"
       send "n\n"
