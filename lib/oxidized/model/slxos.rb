41db9e1a769b7a01eda55f0ddbd53f18cbdb71fa
Mikaël Mantel <mika@mikael.mantel@protonmail.com>
2017-10-05 11:15:23 +0200

    Replace tabs with spaces

--- lib/oxidized/model/slxos.rb
+++ lib/oxidized/model/slxos.rb
@@ -12,24 +12,24 @@ class SLXOS < Oxidized::Model
 
   cmd 'show chassis' do |cfg|
     cfg.encode!("UTF-8", :invalid => :replace, :undef => :replace) #sometimes ironware returns broken encoding
-	cfg.gsub! /.*Power Usage.*/, '' #remove unwanted lines power usage
-	cfg.gsub! /Time A(live|wake).*/, '' #remove unwanted lines time alive/awake
+        cfg.gsub! /.*Power Usage.*/, '' #remove unwanted lines power usage
+        cfg.gsub! /Time A(live|wake).*/, '' #remove unwanted lines time alive/awake
     cfg.gsub! /([\[]*)1([\]]*)<->([\[]*)2([\]]*)(<->([\[]*)3([\]]*))*/, ''
 
     comment cfg
   end
 
   cmd 'show system' do |cfg|
-	cfg.gsub! /Up Time.*/, '' #removes uptime line
-	cfg.gsub! /Current Time.*/, '' #remove current time line
-	cfg.gsub! /.*speed is.*/, '' #removes fan speed lines
+        cfg.gsub! /Up Time.*/, '' #removes uptime line
+        cfg.gsub! /Current Time.*/, '' #remove current time line
+        cfg.gsub! /.*speed is.*/, '' #removes fan speed lines
 
     comment cfg
   end
 
   cmd 'show slots' do |cfg|
     cfg.gsub! /^-*^$/, '' # some slx devices are fixed config
-	cfg.gsub! /syntax error: element does not exist/, '' # same as above
+        cfg.gsub! /syntax error: element does not exist/, '' # same as above
 
     comment cfg
   end
@@ -55,7 +55,7 @@ class SLXOS < Oxidized::Model
     end
     post_login ''
     post_login 'terminal length 0'
-	pre_logout 'exit'
+        pre_logout 'exit'
   end
 
 end
