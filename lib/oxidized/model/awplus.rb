f216292fd6841fb6ea93dc9918a7be63ccff2cf6
Matt Read <4696925+mattie47@users.noreply.github.com>
2018-01-04 14:22:42 +1300

    Add Allied Telesis to supported models
    Feature support included:
    
    - Telnet and SSH
    -- Enable/Secret Password
    - Device info from "show system" command
    -- Dynamic output e.g. Device Uptime is removed.
    - Var: remove_secret
    - User-defined terminal length
    -- User is not required to "terminal no length" and can keep configured pager length.

--- lib/oxidized/model/awplus.rb
+++ lib/oxidized/model/awplus.rb
@@ -1,19 +1,27 @@
 class AWPlus < Oxidized::Model
 
-#Allied Telesis Alliedware Plus Model#
-#https://www.alliedtelesis.com/products/software/AlliedWare-Plus
+  #Allied Telesis Alliedware Plus Model#
+  #https://www.alliedtelesis.com/products/software/AlliedWare-Plus
   
   prompt /^(\r?[\w.@:\/-]+[#>]\s?)$/
   comment  '! '
 
-#Avoids needing "term length 0" however may print other pager characters in config
-#  expect /--More--/ do |data, re|
-#    send ' '
-#    data.sub re, ''
-#  end
+  #Avoids needing "term length 0" to display full config file. 
+    expect /--More--/ do |data, re|
+      send ' '
+      data.sub re, ''
+    end
 
-#Remove passwords from config file.
-#Add vars "remove_secret: true" to global oxidized config file to enable. 
+  #Removes gibberish pager output e.g. VT100 escape codes
+  cmd :all do |cfg|
+    cfg.gsub! /\e\[K/, ''         # example how to handle pager - cleareol EL0
+    cfg.gsub! /\e\[7m\e\[m/, ''   # example how to handle pager - Reverse SGR7
+    cfg.gsub! /\r/, ''  # Filters rogue ^M - see issue #415
+    cfg.each_line.to_a[1..-2].join
+  end
+
+  #Remove passwords from config file.
+  #Add vars "remove_secret: true" to global oxidized config file to enable. 
 
   cmd :secret do |cfg|
     cfg.gsub! /^(snmp-server community).*/, '\\1 <configuration removed>'
@@ -26,37 +34,37 @@ class AWPlus < Oxidized::Model
     cfg
   end
 
-#Adds "Show system" output to start of config. 
-   cmd 'Show System' do |cfg|
-#Removes the following lines from "show system" in output file. This ensures oxidized diffs are meaningful. 
-    cfg.gsub! /System Status\s*.*/, ''
-    cfg.gsub! /RAM\s*:.*/, ''
-    cfg.gsub! /Uptime\s*:.*/, ''
-    cfg.gsub! /Flash\s*:.*/, ''
-    cfg.gsub! /Current software\s*:.*/, ''
-    cfg.gsub! /Software version\s*:.*/, ''
-    cfg.gsub! /Build date\s*:.*/, ''
-    cfg.gsub! /^$\n/, '' #Remove blank lines in show sys caused by removal of above output.
-    comment cfg.insert(0,"--------------------------------------------------------------------------------! \n")
-#	Unhash below to write a comment in the config file.
-    cfg.insert(0,"Starting: Show system cmd \n")
-    comment cfg
-#	Unhash below to write a comment in the config file.
-   cfg << "\n\nEnding: show system cmd"
-    comment cfg << "\n--------------------------------------------------------------------------------! \n\n"
-    end
+  #Adds "Show system" output to start of config. 
 
-#Actually get the devices running config#
+  cmd 'Show System' do |cfg|
+    comment cfg.insert(0,"--------------------------------------------------------------------------------! \n")
+    #Unhash below to write a comment in the config file.
+    cfg.insert(0,"Starting: Show system cmd \n")
+    cfg << "\n \nEnding: show system cmd"
+    comment cfg << "\n--------------------------------------------------------------------------------! \n \n"
+    #Removes the following lines from "show system" in output file. This ensures oxidized diffs are meaningful. 
+    comment cfg.each_line.reject { |line| 
+                                    line.match /^$\n/ or #Remove blank lines in "sh sys"
+                                    line.match /System Status\s*.*/ or
+                                    line.match /RAM\s*:.*/ or 
+                                    line.match /Uptime\s*:.*/ or 
+                                    line.match /Flash\s*:.*/ or 
+                                    line.match /Current software\s*:.*/ or 
+                                    line.match /Software version\s*:.*/ or 
+                                    line.match /Build date\s*:.*/ }.join
+  end
+     
+  #Actually get the devices running config#
   cmd 'show running-config' do |cfg|
     cfg
   end
   
-#Config required for telnet to detect username prompt
+  #Config required for telnet to detect username prompt
   cfg :telnet do
     username /login:\s/
     end
 
-#Main login config
+  #Main login config
     cfg :telnet, :ssh do
     post_login do
       if vars :enable
@@ -66,13 +74,12 @@ class AWPlus < Oxidized::Model
       else
         cmd 'enable' # Required for Priv-Exec users without enable PW to be put into "enable mode". 
       end
-      cmd 'terminal length 0' #set so the entire config is output without intervention.
+#      cmd 'terminal length 0' #set so the entire config is output without intervention.
     end
     pre_logout do
-      cmd 'terminal no length' #Sets term length back to default on exit. 
+#      cmd 'terminal no length' #Sets term length back to default on exit. 
       send  "exit\r\n"
     end
   end  
 
 end
-
