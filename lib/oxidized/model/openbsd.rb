a355c442ea91988487df432724a814328dcfacd9
Albert Martinez <a.martinez@mrgc.net>
2018-05-30 17:12:45 +0200

    Update openbsd.rb
    Routing domains info

--- lib/oxidized/model/openbsd.rb
+++ lib/oxidized/model/openbsd.rb
@@ -12,40 +12,55 @@ class Openbsd < Oxidized::Model
     "\n+++++++++++++++++++++++++++++++++++++++++ #{comment} ++++++++++++++++++++++++++++++++++++++++++++++\n" 
   end
 
+  def add_small_comment comment
+    "\n=============== #{comment} ===============\n" 
+  end
+  
   cmd :all do |cfg|
     cfg.each_line.to_a[1..-2].join
   end
   
   #Issue the show commands
   pre do
-    cfg = add_comment 'HOSTNAME'
+    cfg = add_comment 'HOSTNAME FILE'
     cfg += cmd 'cat /etc/myname'
     
     cfg += add_comment 'HOSTS FILE'
     cfg += cmd 'cat /etc/hosts'
     
-    cfg += add_comment 'INTERFACES'
+    cfg += add_comment 'INTERFACE FILES'
     cfg += cmd 'tail +n 1 /etc/hostname.*'
     
     cfg += add_comment 'RESOLV.CONF FILE'
     cfg += cmd 'cat /etc/resolv.conf'
     
-    cfg += add_comment 'NTP.CONF'
+    cfg += add_comment 'NTP.CONF FILE'
     cfg += cmd 'cat /etc/ntp.conf'
     
-    cfg += add_comment 'IP Routes'
-    cfg += cmd 'netstat -rn'
+    cfg += add_comment 'IP ROUTES PER ROUTING DOMAIN'
+    cfg += add_small_comment 'Routing Domain 0'
+    cfg += cmd 'route -T0 exec netstat -rn'
+    cfg += add_small_comment 'Routing Domain 1'
+    cfg += cmd 'route -T1 exec netstat -rn'
+    cfg += add_small_comment 'Routing Domain 2'
+    cfg += cmd 'route -T2 exec netstat -rn'
+    cfg += add_small_comment 'Routing Domain 3'
+    cfg += cmd 'route -T3 exec netstat -rn'
+    cfg += add_small_comment 'Routing Domain 4'
+    cfg += cmd 'route -T4 exec netstat -rn'
+    cfg += add_small_comment 'Routing Domain 5'
+    cfg += cmd 'route -T5 exec netstat -rn'
     
-    cfg += add_comment 'SNMP settings'
+    cfg += add_comment 'SNMP FILE'
     cfg += cmd 'cat /etc/snmpd.conf'
     
-    cfg += add_comment 'PF'
+    cfg += add_comment 'PF FILE'
     cfg += cmd 'cat /etc/pf.conf'
     
-    cfg += add_comment 'MOTD'
+    cfg += add_comment 'MOTD FILE'
     cfg += cmd 'cat /etc/motd'
     
-    cfg += add_comment 'PASSWD'
+    cfg += add_comment 'PASSWD FILE'
     cfg += cmd 'cat /etc/passwd'
     
   end
