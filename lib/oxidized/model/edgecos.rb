5cfdbaa959b86d2c8e65963a828ab309302c9020
Bartek <bartek@sq9mev.info>
2018-06-05 15:34:20 +0200

    edgecos overly verbose memory and uptime (#1366)

--- lib/oxidized/model/edgecos.rb
+++ lib/oxidized/model/edgecos.rb
@@ -13,15 +13,8 @@ class EdgeCOS < Oxidized::Model
 
   cmd 'show running-config'
 
-  cmd 'show access-list tcam-utilization' do |cfg|
-    comment cfg
-  end
-
-  cmd 'show memory' do |cfg|
-    comment cfg
-  end
-
   cmd 'show system' do |cfg|
+    cfg.gsub! /^\s*System Up Time\s*:.*\n/i, ''
     comment cfg
   end
 
