5a936746a951d785b094924535b8a491c94705af
rjmeredeth <rjmeredeth@meredeth.com>
2018-09-06 21:01:52 -0500

    Model for Comtrol Industrial Switches

---
+++ lib/oxidized/model/comtrol.rb
@@ -0,0 +1,44 @@
+class Comtrol < Oxidized::Model
+  # Used in Comtrol Industrial Switches, such as ES8510
+
+  # Typical prompt "<hostname>#"
+  prompt /([#>]\s?)$/
+  comment  '! '
+
+
+  # how to handle pager
+  expect /--More--+\s$/ do |data, re|
+    send ' '
+    data.sub re, ''
+  end
+
+
+
+  cmd :all do |cfg|
+    cfg.gsub! /^.*\e7/, ''
+  end
+ 
+  cmd 'show version'
+
+  cmd 'show running-config'
+
+  cfg :telnet do
+    username /^User name:/i
+    password /^Password:/i
+  end
+
+  cfg :telnet, :ssh do
+    if vars :enable
+      post_login do
+        send "enable\n"
+        # Interpret enable: true as meaning we won't be prompted for a password
+        unless vars(:enable).is_a? TrueClass
+          expect /[pP]assword:\s?$/
+          send vars(:enable) + "\n"
+        end
+        expect /^.+[#]\s?$/
+      end
+    end
+    pre_logout 'exit'
+  end
+end
