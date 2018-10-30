197735a075c4d9e7f2eef971025f83cec624e917
Lewis Green <lewis.green@vive.tech>
2018-10-30 16:09:37 +0000

    Added Firebrick Support

--- lib/oxidized/model/firebrick.rb
+++ lib/oxidized/model/firebrick.rb
@@ -1,26 +1,29 @@
 class Firebrick < Oxidized::Model
   # Firebrick #
+  prompt /\x0a\x1b\x5b\x32\x4b\x0d.*>\s/
 
-  prompt /.*[>]/
+  cmd :all do |cfg|
+    # sometimes ironware inserts arbitrary whitespace after commands are
+    # issued on the CLI, from run to run.  this normalises the output.
+    cfg.each_line.to_a[1..-2].drop_while { |e| e.match /^\s+$/ }.join
+  end
 
   cmd 'show status' do |cfg|
     cfg.gsub! /Status/, ''
     cfg.gsub! /------/, ''
     cfg.gsub! /Uptime.*/, ''
     cfg.gsub! /Current time.*/, ''
-    cfg.gsub! /RAM.*/, ''
+    cfg.gsub! /RAM.*/, ''
     cfg.gsub! /Warranty.*/, ''
 
     comment cfg
   end
 
-  cmd 'show configuration' do |cfg|
-    cfg
-  end
+  cmd 'show configuration'
 
   cfg :telnet do
-    username /User(name)?:\s?/
-    password /^Password:\s?/
+    username /Username:\s?/
+    password /Password:\s?/
   end
 
   cfg :telnet, :ssh do
