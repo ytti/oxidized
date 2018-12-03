8f562307e13b83f5b549025fb1e280624b0a3c05
Saku Ytti <saku@ytti.fi>
2018-12-03 09:28:08 +0200

    ciscospark refactor 2/2

--- lib/oxidized/hook/ciscosparkdiff.rb
+++ lib/oxidized/hook/ciscosparkdiff.rb
@@ -19,25 +19,25 @@ class CiscoSparkDiff < Oxidized::Hook
       config.api_key = cfg.accesskey
       config.proxy = cfg.proxy if cfg.has_key?('proxy')
     end
-    client = CiscoSpark::Room.new(id: cfg.space)
-    client.fetch
+    room = CiscoSpark::Room.new(id: cfg.space)
     log "Connected"
+
     if cfg.diff? ? cfg.diff : true
       gitoutput = ctx.node.output.new
       diff = gitoutput.get_diff ctx.node, ctx.node.group, ctx.commitref, nil
       title = ctx.node.name.to_s
       log "Posting diff as snippet to #{cfg.space}"
-      room = CiscoSpark::Room.new(id: cfg.space)
       room.send_message CiscoSpark::Message.new(text: 'Device ' + title + ' modified:' + "\n" + diff[:patch].lines.to_a[4..-1].join)
     end
+
     if cfg.message?
       log cfg.message
       msg = cfg.message % { node: ctx.node.name.to_s, group: ctx.node.group.to_s, commitref: ctx.commitref, model: ctx.node.model.class.name.to_s.downcase }
       log msg
       log "Posting message to #{cfg.space}"
-      room = CiscoSpark::Room.new(id: cfg.space)
       room.send_message CiscoSpark::Message.new(text: msg)
     end
+
     log "Finished"
   end
 end
