f08ee5117c4fa8cead953a1bcc2dcc820667cdcc
Andrew <a.baznikin@gmail.com>
2018-12-17 16:23:32 +0700

    Update ciscovpn3k.rb

--- lib/oxidized/model/ciscovpn3k.rb
+++ lib/oxidized/model/ciscovpn3k.rb
@@ -1,4 +1,4 @@
-class VPN3000 < Oxidized::Model
+class CiscoVPN3k < Oxidized::Model
   # Used in Cisco VPN3000 concentrators
   # it have buggy code 227 reply with whitespace before trailing bracket
   # "227 Passive mode OK (172,16,0,9,4,9 )"
