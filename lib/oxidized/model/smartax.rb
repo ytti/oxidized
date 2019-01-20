841e461e25fb89d74a79d28b2f37f6751c48d9e2
nyash <nyashh@gmail.com>
2019-01-20 22:17:58 +0100

    Create smartax.rb
    Huawei SmartAX model.

---
+++ lib/oxidized/model/smartax.rb
@@ -0,0 +1,22 @@
+class SmartAX < Oxidized::Model
+
+  prompt /^([\w.-]+[>#])$/
+
+  comment '#' 
+
+  cfg :telnet do
+    username /^>>User name:$/
+    password /^>>User password:$/
+  end
+
+  cfg :ssh, :telnet do
+    post_login "enable" 
+    post_login "undo interactive"
+    post_login "undo smart"
+    post_login "scroll"
+    pre_logout "quit"
+  end 
+
+  cmd 'display current-configuration'
+
+end
