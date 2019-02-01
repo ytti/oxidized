382120c18576513f6d4185d9fdb2444b1ea610f7
starrsl <47113781+starrsl@users.noreply.github.com>
2019-02-01 11:55:06 +0000

    Update tdre.rb

--- lib/oxidized/model/tdre.rb
+++ lib/oxidized/model/tdre.rb
@@ -1,32 +1,32 @@
 class TDRE < Oxidized::Model
 
-    prompt /^>$/ 
-    cmd "get -f"
+  prompt /^>$/ 
+  cmd "get -f"
 
-	def ssh
-	  @input.class.to_s.match(/SSH/)
-    end
+  def ssh
+    @input.class.to_s.match(/SSH/)
+  end
 
-    expect /^>.+$/ do |data, re|
-        send "\r" if ssh
-        data.sub re, ''
+  expect /^>.+$/ do |data, re|
+    send "\r" if ssh
+    data.sub re, ''
+  end
+
+  cmd :all do |cfg|
+    if ssh
+    cfg = cfg.lines.to_a[5..-4].join
+    else
+    cfg = cfg.lines.to_a[1..-4].joim
     end
-	
-    cmd :all do |cfg|
-        if ssh
-            cfg = cfg.lines.to_a[5..-4].join
-        else
-            cfg = cfg.lines.to_a[1..-4].joim
-        end
-    end
-    
-    cfg :telnet do
-        username /^Username:/
-        password /^Password:/
-    end
-    
-    cfg :telnet, :ssh do
-      pre_logout "DISCONNECT\r" 
-    end
+  end
+
+  cfg :telnet do
+    username /^Username:/
+    password /^Password:/
+  end
+
+  cfg :telnet, :ssh do
+    pre_logout "DISCONNECT\r" 
+  end
 
 end
