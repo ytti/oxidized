518617ee65909eaff04b8c5df66e46fa876b1cad
starrsl <47113781+starrsl@users.noreply.github.com>
2019-02-01 11:31:20 +0000

    Create tdre.rb
    Adds support for OneAccess/TDRE devices.

---
+++ lib/oxidized/model/tdre.rb
@@ -0,0 +1,32 @@
+class TDRE < Oxidized::Model
+
+    prompt /^>$/ 
+    cmd "get -f"
+
+	def ssh
+	  @input.class.to_s.match(/SSH/)
+    end
+
+    expect /^>.+$/ do |data, re|
+        send "\r" if ssh
+        data.sub re, ''
+    end
+	
+    cmd :all do |cfg|
+        if ssh
+            cfg = cfg.lines.to_a[5..-4].join
+        else
+            cfg = cfg.lines.to_a[1..-4].joim
+        end
+    end
+    
+    cfg :telnet do
+        username /^Username:/
+        password /^Password:/
+    end
+    
+    cfg :telnet, :ssh do
+      pre_logout "DISCONNECT\r" 
+    end
+
+end
