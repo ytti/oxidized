a65771ec0e8158763f3f602acf4d8af0320cf0d1
Saku Ytti <saku@ytti.fi>
2019-03-26 17:23:57 +0200

    Potially fix #1758

--- lib/oxidized/string.rb
+++ lib/oxidized/string.rb
@@ -5,17 +5,17 @@ module Oxidized
 
     # @return [Oxidized::String] copy of self with last line removed
     def cut_tail(lines = 1)
-      Oxidized::String.new each_line.to_a[0..-1 - lines].join
+      Oxidized::String.new each_line.to_a[0..-1 - lines]&.join || ""
     end
 
     # @return [Oxidized::String] copy of self with first line removed
     def cut_head(lines = 1)
-      Oxidized::String.new each_line.to_a[lines..-1].join
+      Oxidized::String.new each_line.to_a[lines..-1]&.join || ""
     end
 
     # @return [Oxidized::String] copy of self with first and last lines removed
     def cut_both(head = 1, tail = 1)
-      Oxidized::String.new each_line.to_a[head..-1 - tail].join
+      Oxidized::String.new each_line.to_a[head..-1 - tail]&.join || ""
     end
 
     # sets @cmd and @name unless @name is already set
