e69335a7cffd7b855f31b815ac9d103efb2b2b26
Prashanth Gopinath <prashanth.gopinath@gmail.com>
2019-05-06 19:51:22 +0530

    Fix to get data from node.vars
    Added code to convert param tostring, else the node level "vars" was not returned.

--- lib/oxidized/config/vars.rb
+++ lib/oxidized/config/vars.rb
@@ -2,7 +2,7 @@ module Oxidized::Config::Vars
   # convenience method for accessing node, group or global level user variables
   # nil values will be ignored
   def vars(name)
-    r = @node.vars[name] unless @node.vars.nil?
+    r = @node.vars[name.to_s] unless @node.vars.nil?
     if Oxidized.config.groups.has_key?(@node.group)
       r ||= Oxidized.config.groups[@node.group].vars[name.to_s] if Oxidized.config.groups[@node.group].vars.has_key?(name.to_s)
     end
