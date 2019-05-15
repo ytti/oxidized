79be08fd3e4bdf8ccd65f2ac889a2d805126c945
Brian Candler <b.candler@pobox.com>
2019-05-15 07:32:38 +0100

    Strip line "# received packet from x.x.x.x bad format" from RouterOS
    This message intermittently appears and disappears when RouterOS
    is running VRRP and there is also CARP traffic on the same network

--- lib/oxidized/model/routeros.rb
+++ lib/oxidized/model/routeros.rb
@@ -29,6 +29,7 @@ class RouterOS < Oxidized::Model
     cmd run_cmd do |cfg|
       cfg.gsub! /\\\r?\n\s+/, '' # strip new line
       cfg.gsub! /# inactive time\r\n/, '' # Remove time based system comment
+      cfg.gsub! /# received packet from \S+ bad format\r\n/, '' # Remove intermittent VRRP/CARP collision comment
       cfg = cfg.split("\n").reject { |line| line[/^#\s\w{3}\/\d{2}\/\d{4}.*$/] }
       cfg.join("\n") + "\n"
     end
