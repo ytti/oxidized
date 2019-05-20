bea677ceab778d1c7e6d10c48a16462a6950cbf7
Exordian <jakob.englisch@catalysts.cc>
2019-05-20 08:02:03 +0200

    Procurve: remove show system power-supply command (#1798)

--- lib/oxidized/model/procurve.rb
+++ lib/oxidized/model/procurve.rb
@@ -49,10 +49,6 @@ class Procurve < Oxidized::Model
     comment cfg
   end
 
-  cmd 'show system power-supply' do |cfg|
-    comment cfg
-  end
-
   cmd 'show interfaces transceiver' do |cfg|
     comment cfg
   end
