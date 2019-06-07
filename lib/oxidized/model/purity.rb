67efc0d464fa42bce4b6e2a758763e176ccdbcb9
Roland Scheike <git@bayreuth.tk>
2019-06-07 11:32:21 +0200

    Create purity.rb
    Add support for Pure Storage

---
+++ lib/oxidized/model/purity.rb
@@ -0,0 +1,12 @@
+class Purity < Oxidized::Model
+  # Pure Storage Purity OS
+  
+  prompt /\w+@\S+(\s+\S+)*\s?>\s?$/
+  comment '# '
+  
+  cmd 'pureconfig list'
+  
+  cfg :ssh do
+	pre_logout 'exit'
+  end
+end
