e80d28549187f2ac0bda81048e452201ee82f05f
Saku Ytti <saku@ytti.fi>
2019-09-03 18:38:29 +0300

    Fix rubocop warning

--- lib/oxidized/config/vars.rb
+++ lib/oxidized/config/vars.rb
@@ -1,7 +1,7 @@
 module Oxidized::Config::Vars
   # convenience method for accessing node, group or global level user variables
   def vars(name)
-    if @node.vars && @node.vars.has_key?(name)
+    if @@node.vars&.has_key?(name)
       @node.vars[name]
     elsif Oxidized.config.groups.has_key?(@node.group) && Oxidized.config.groups[@node.group].vars.has_key?(name.to_s)
       Oxidized.config.groups[@node.group].vars[name.to_s]
