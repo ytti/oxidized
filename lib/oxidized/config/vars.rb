93d842e29a7f7f3099329db762061c1daa23aa59
Rauno Tuul <rauno@tuul.eu>
2019-09-03 16:12:04 +0300

    removed double checking the first condition

--- lib/oxidized/config/vars.rb
+++ lib/oxidized/config/vars.rb
@@ -2,7 +2,7 @@ module Oxidized::Config::Vars
   # convenience method for accessing node, group or global level user variables
   def vars(name)
     if @node.vars && @node.vars.has_key?(name)
-      @node.vars[name] if @node.vars && @node.vars.has_key?(name)
+      @node.vars[name]
     elsif Oxidized.config.groups.has_key?(@node.group) && Oxidized.config.groups[@node.group].vars.has_key?(name.to_s)
       Oxidized.config.groups[@node.group].vars[name.to_s]
     elsif Oxidized.config.models.has_key(@node.model.class.name.to_s.downcase) && Oxidized.config.models[@node.model.class.name.to_s.downcase].vars.has_key?(name.to_s)
