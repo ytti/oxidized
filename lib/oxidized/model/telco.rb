ba69db5fe30dba622bba09d7d279ccf1b8221264
Skyler Blumer <skyler.blumer@zitomedia.com>
2019-09-30 19:41:08 +0000

    Added telco.rb - Support for T-Marc 3306

---
+++ lib/oxidized/model/telco.rb
@@ -0,0 +1,22 @@
+class TELCO < Oxidized::Model
+  prompt /^(\r?[\w.@_()-]+[#]\s?)$/
+  comment '! '
+
+  cmd :all do |cfg|
+    cfg.each_line.to_a[2..-2].join.delete("\n")
+  end
+
+  cmd 'show running-config' do |cfg|
+    cfg
+  end
+
+  cfg :ssh, :telnet do
+    post_login 'terminal length 0'
+    pre_logout 'exit'
+  end
+
+  cfg :telnet do
+    username /^Username:/
+    password /^Password:/
+  end
+end
