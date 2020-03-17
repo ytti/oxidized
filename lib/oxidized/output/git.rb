d96a592be605149f8a4f8d45e8b35e1ed8f3049c
Saku Ytti <saku@ytti.fi>
2020-03-17 16:47:36 +0200

    Rugged::Walker#push now wants oid_string
    Previously we gave it instance of Rugged::Commit object
    
    Many thanks to Greg DiFiore for providing device to reproduce this on.
    
    Fixed #2049

--- lib/oxidized/output/git.rb
+++ lib/oxidized/output/git.rb
@@ -74,7 +74,7 @@ module Oxidized
       repo = Rugged::Repository.new repo
       walker = Rugged::Walker.new(repo)
       walker.sorting(Rugged::SORT_DATE)
-      walker.push(repo.head.target)
+      walker.push(repo.head.target.oid)
       i = -1
       tab = []
       walker.each do |commit|
