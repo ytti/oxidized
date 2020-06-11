713dc96ba8ae0fc78b3e841c7a26bc7143381f94
glaubway <ast.toggie@gmail.com>
2020-06-11 19:06:53 +0300

    Update zteolt.rb

--- lib/oxidized/model/zteolt.rb
+++ lib/oxidized/model/zteolt.rb
@@ -1,5 +1,6 @@
 class ZTEOLT < Oxidized::Model
-# Tested with C320 and C300 olt, firware 1.2.5P3 and 2.1.0
+  # Tested with C320 and C300 olt, firware 1.2.5P3 and 2.1.0
+  
   prompt /^([\w.@()-]+[#>]\s?)$/
   comment  '! '
 
