1ff893a2fe90b8110d7ec42d689a59e80812650b
Marco Túlio Zuquim <mtulio.z@gmail.com>
2020-08-13 15:52:42 -0300

    datacom.rb: Fix login prompt and pagination issues - prompt issue: new DmOS firmware shows a welcome message post login; - prompt sol.: custom model RegEx (added [\n]? in the beginning); - pagination issue: couldn't make expect(regex) method to paginate; - pagination sol.: disabling DataCom terminal pagination during backup;

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -27,6 +27,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/).
 - fixed an issue where Oxidized could not pull config from XOS-devices operating in stacked mode (@DarkCatapulter)
 - fixed an issue where Oxidized could not pull config from XOS-devices that have not saved their configuration (@DarkCatapulter)
 - improved scrubbing of show chassis in ironware model (@michaelpsomiadis)
+- fixed an issue where Oxidized was unable to match post login prompt RegEx in new DmOS firmware (@Zuquim)
+- fixed an issue where Oxidized was unable to properly paginate DataCom devices' terminal output (@Zuquim)
 
 ## [0.28.0 - 2020-05-18]
 
--- lib/oxidized/model/datacom.rb
+++ lib/oxidized/model/datacom.rb
@@ -1,16 +1,30 @@
 class DataCom < Oxidized::Model
+  # editing model specific prompt RegEx, adding: [\n]?
+  # to workaround the new DmOS post login welcome message
+  prompt /([\n]?[\w-]+[(]*[\w-]*[)]*[#>][\s]?)$/
+  # @legacy => false: new DmOS firmware | true: old OS firmware
+  @legacy = false
   comment '! '
 
-  expect /^--More--\s+$/ do |data, re|
-    send ' '
-    data.sub re, ''
-  end
+# was not able to make expect method work for pagination
+#  expect /^\s*--More--\s*$/ do |data, re|
+#    send ' '
+#    data.sub re, ''
+#  end
 
   cmd :all do |cfg|
-    cfg.cut_head.cut_both.cut_tail
+    cfg.cut_both
   end
 
   cmd 'show firmware' do |cfg|
+    # since 'show firmware' does not paginate in neither firmware versions,
+    # chose to use it's output to distinguish between the old and new OS
+    if cfg.lines.first =~ /^Running firmware:\s*$/  # Old OS firmware
+      @legacy = true
+      cmd "config\nno terminal paging\nexit"
+    else  # New DmOS firmware
+      cmd "paginate false"
+    end
     comment cfg
   end
 
@@ -20,6 +34,10 @@ class DataCom < Oxidized::Model
 
   cmd 'show running-config' do |cfg|
     cfg.cut_head
+  end
+
+  if @legacy == true
+    cmd "config\nterminal paging\nexit"
   end
 
   cfg :ssh do
