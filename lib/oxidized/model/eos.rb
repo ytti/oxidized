7c84346215c6d265bb3af491c66e5d48d378f7df
Piet Hugen <phugen@schubergphilis.com>
2020-09-25 11:18:47 +0200

    remove snmp community and ospf key from eos config

--- lib/oxidized/model/eos.rb
+++ lib/oxidized/model/eos.rb
@@ -11,11 +11,13 @@ class EOS < Oxidized::Model
 
   cmd :secret do |cfg|
     cfg.gsub! /^(snmp-server community).*/, '\\1 <configuration removed>'
+    cfg.gsub! /^(snmp-server host .*) (\S+\Z)/, '\\1 <configuration removed>'
     cfg.gsub! /(secret \w+) (\S+).*/, '\\1 <secret hidden>'
     cfg.gsub! /(password \d+) (\S+).*/, '\\1 <secret hidden>'
     cfg.gsub! /^(enable secret).*/, '\\1 <configuration removed>'
     cfg.gsub! /^(tacacs-server key \d+).*/, '\\1 <configuration removed>'
     cfg.gsub! /( {6}key) (\h+ 7) (\h+).*/, '\\1 <secret hidden>'
+    cfg.gsub! /(key \d) (\S+)/, '\\1 <secret hidden>'
     cfg
   end
 
