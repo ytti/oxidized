1e07679010ea943fb78b21347fe0af21d85d14de
martydingo <49862656+martydingo@users.noreply.github.com>
2020-10-02 10:34:51 +0100

    Create cambiumepmp.rb

---
+++ lib/oxidized/model/cambiumepmp.rb
@@ -0,0 +1,18 @@
+class CambiumePMP < Oxidized::Model
+  # Cambium ePMP Radios
+
+  # prompt /\(.*\)>/
+  prompt /.*>/
+
+  cmd :all do |cfg|
+    cfg.cut_both
+  end
+
+  pre do 
+    cmd 'config show json'
+  end
+
+  cfg :ssh do
+    pre_logout 'exit'
+  end
+end
