e6817a2ded7e8025d2c392863d37a40b846e7766
George Kelly <george.kelly@ukfast.co.uk>
2020-10-14 10:03:58 +0100

    Added config option for new run command endpoint

--- lib/oxidized/config.rb
+++ lib/oxidized/config.rb
@@ -17,23 +17,24 @@ module Oxidized
       asetus = Asetus.new(name: 'oxidized', load: false, key_to_s: true)
       Oxidized.asetus = asetus
 
-      asetus.default.username      = 'username'
-      asetus.default.password      = 'password'
-      asetus.default.model         = 'junos'
-      asetus.default.resolve_dns   = true # if false, don't resolve DNS to IP
-      asetus.default.interval      = 3600
-      asetus.default.use_syslog    = false
-      asetus.default.debug         = false
-      asetus.default.threads       = 30
-      asetus.default.timeout       = 20
-      asetus.default.retries       = 3
-      asetus.default.prompt        = /^([\w.@-]+[#>]\s?)$/
-      asetus.default.rest          = '127.0.0.1:8888' # or false to disable
-      asetus.default.next_adds_job = false            # if true, /next adds job, so device is fetched immmeiately
-      asetus.default.vars          = {}               # could be 'enable'=>'enablePW'
-      asetus.default.groups        = {}               # group level configuration
-      asetus.default.models        = {}               # model level configuration
-      asetus.default.pid           = File.join(Oxidized::Config::Root, 'pid')
+      asetus.default.username             = 'username'
+      asetus.default.password             = 'password'
+      asetus.default.model                = 'junos'
+      asetus.default.resolve_dns          = true # if false, don't resolve DNS to IP
+      asetus.default.interval             = 3600
+      asetus.default.use_syslog           = false
+      asetus.default.debug                = false
+      asetus.default.threads              = 30
+      asetus.default.timeout              = 20
+      asetus.default.retries              = 3
+      asetus.default.prompt               = /^([\w.@-]+[#>]\s?)$/
+      asetus.default.rest                 = '127.0.0.1:8888' # or false to disable
+      asetus.default.run_command_endpoint = false # if true enable the /node/run-command/:node endpoint
+      asetus.default.next_adds_job        = false # if true, /next adds job, so device is fetched immmeiately
+      asetus.default.vars                 = {} # could be 'enable'=>'enablePW'
+      asetus.default.groups               = {} # group level configuration
+      asetus.default.models               = {} # model level configuration
+      asetus.default.pid                  = File.join(Oxidized::Config::Root, 'pid')
 
       asetus.default.crash.directory = File.join(Oxidized::Config::Root, 'crashes')
       asetus.default.crash.hostnames = false
