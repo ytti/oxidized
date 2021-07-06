a94cf5dd266e19b80012c3ebff9cac34b8dbac49
Rinse Kloek <rkloek@ponea.kabelfoon.intern>
2021-07-06 14:45:18 +0200

    Nokia ISAM

---
+++ lib/oxidized/model/sros_isam.rb
@@ -0,0 +1,73 @@
+class SROS_ISAM < Oxidized::Model
+  #
+  # Nokia SR OS (TiMOS) (formerly TiMetra, Alcatel, Alcatel-Lucent).
+  # Used in 7705 SAR, 7210 SAS, 7450 ESS, 7750 SR, 7950 XRS, and NSP.
+  #
+
+  comment  '# '
+
+  prompt /^([-\w.:>*]+\s?[#>]\s?)$/
+
+  cmd :all do |cfg, cmdstring|
+    new_cfg = comment "COMMAND: #{cmdstring}\n"
+    cfg.gsub! /# Finished .*/, ''
+    cfg.gsub! /# Generated .*/, ''
+    cfg.delete! "\r"
+    new_cfg << cfg.cut_both
+  end
+
+  # Show the ports
+  #
+  cmd 'show port' do |cfg|
+    comment cfg
+  end
+
+  # Show the equipment shelf
+  #
+  cmd 'show equipment shelf' do |cfg|
+    comment cfg
+  end
+
+  # Show the equipment slot
+  #
+  cmd 'show equipment slot' do |cfg|
+    comment cfg
+  end
+
+  # Show the equipment transceiver-inventory
+  #
+  cmd 'show equipment transceiver-inventory' do |cfg|
+    comment cfg
+  end
+
+  # Show the equipment ont interface
+  #
+  cmd 'show equipment ont interface' do |cfg|
+    comment cfg
+  end
+
+  # Show the equipment ont slot
+  #
+  cmd 'show equipment ont slot' do |cfg|
+    comment cfg
+  end
+
+  # Show configuration
+  cmd 'info configure' do |cfg|
+    cfg
+  end
+
+
+  cfg :telnet do
+    username /^Login: /
+    password /^Password: /
+  end
+
+  cfg :telnet, :ssh do
+    post_login 'environment no terminal-timeout'
+    post_login 'environment print no-more'
+    post_login 'environment inhibit-alarms'
+    pre_logout 'logout'
+  end
+end
+
