e6c2969d59b866d7343db058bf06837336035350
Per von Zweigbergk <pvz@pvz.pp.se>
2021-09-30 13:04:59 +0200

    Move xmlcomment into model.rb

--- lib/oxidized/model/opnsense.rb
+++ lib/oxidized/model/opnsense.rb
@@ -2,24 +2,6 @@ class OpnSense < Oxidized::Model
   # minimum required permissions: "System: Shell account access"
   # must enable SSH and password-based SSH access
 
-  def xmlcomment(str)
-    # XML Comments start with <!-- and end with -->
-    #
-    # Because it's illegal for the first or last characters of a comment
-    # to be a -, i.e. <!--- or ---> are illegal, and also to improve
-    # readability, we add extra spaces after and before the beginning
-    # and end of comment markers.
-    #
-    # Also, XML Comments must not contain --. So we put a space between
-    # any double hyphens, by replacing any - that is followed by another -
-    # with '- '
-    data = ''
-    str.each_line do |line|
-      data << '<!-- ' << str.gsub(/-(?=-)/, '- ').chomp << " -->\n"
-    end
-    data
-  end
-
   cmd 'cat /conf/config.xml' do |cfg|
     cfg.gsub! /\s<revision>\s*<time>\d*<\/time>\s*.*\s*.*\s*<\/revision>/, ''
     cfg.gsub! /\s<last_rule_upd_time>\d*<\/last_rule_upd_time>/, ''
--- lib/oxidized/model/pfsense.rb
+++ lib/oxidized/model/pfsense.rb
@@ -1,24 +1,6 @@
 class PfSense < Oxidized::Model
   # use other use than 'admin' user, 'admin' user cannot get ssh/exec. See issue #535
 
-  def xmlcomment(str)
-    # XML Comments start with <!-- and end with -->
-    #
-    # Because it's illegal for the first or last characters of a comment
-    # to be a -, i.e. <!--- or ---> are illegal, and also to improve
-    # readability, we add extra spaces after and before the beginning
-    # and end of comment markers.
-    #
-    # Also, XML Comments must not contain --. So we put a space between
-    # any double hyphens, by replacing any - that is followed by another -
-    # with '- '
-    data = ''
-    str.each_line do |line|
-      data << '<!-- ' << str.gsub(/-(?=-)/, '- ').chomp << " -->\n"
-    end
-    data
-  end
-
   cmd :secret do |cfg|
     cfg.gsub! /(\s+<bcrypt-hash>).+?(<\/bcrypt-hash>)/, '\\1<secret hidden>\\2'
     cfg.gsub! /(\s+<password>).+?(<\/password>)/, '\\1<secret hidden>\\2'
