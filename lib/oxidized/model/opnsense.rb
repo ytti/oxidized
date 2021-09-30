ab50c1ced0e592bf8584f78e64e5f6245c6b5175
Per von Zweigbergk <pvz@pvz.pp.se>
2021-09-30 11:37:40 +0200

    PFsense/OPNsense: Add feature to gather version information as a comment at the end of the file

--- lib/oxidized/model/opnsense.rb
+++ lib/oxidized/model/opnsense.rb
@@ -2,10 +2,40 @@ class OpnSense < Oxidized::Model
   # minimum required permissions: "System: Shell account access"
   # must enable SSH and password-based SSH access
 
+  def xmlcomment(str)
+    # XML Comments start with <!-- and end with -->
+    #
+    # Because it's illegal for the first or last characters of a comment
+    # to be a -, i.e. <!--- or ---> are illegal, and also to improve
+    # readability, we add extra spaces after and before the beginning
+    # and end of comment markers.
+    #
+    # Also, XML Comments must not contain --. So we put a space between
+    # any double hyphens, by replacing any - that is followed by another -
+    # with '- '
+    data = ''
+    str.each_line do |line|
+      data << '<!-- ' << str.gsub(/-(?=-)/, '- ').chomp << " -->\n"
+    end
+    data
+  end
+
   cmd 'cat /conf/config.xml' do |cfg|
     cfg.gsub! /\s<revision>\s*<time>\d*<\/time>\s*.*\s*.*\s*<\/revision>/, ''
     cfg.gsub! /\s<last_rule_upd_time>\d*<\/last_rule_upd_time>/, ''
     cfg
+  end
+
+  # The comment output has to be at the end since and XML file may not start
+  # with a comment.
+
+  # This gets the version using the opnsense-version command, or from the
+  # /usr/local/opnsense/version/opnsense file for earlier versions of OPNsense
+  # that lack the opnsense-version command. Newer versions of OPNsense no longer
+  # store the version information in this file, so both versions have to be
+  # supported here for now.
+  cmd 'opnsense-version 2>/dev/null || echo "OPNsense "`cat /usr/local/opnsense/version/opnsense`' do |version|
+    xmlcomment version
   end
 
   cfg :ssh do
--- lib/oxidized/model/pfsense.rb
+++ lib/oxidized/model/pfsense.rb
@@ -1,6 +1,24 @@
 class PfSense < Oxidized::Model
   # use other use than 'admin' user, 'admin' user cannot get ssh/exec. See issue #535
 
+  def xmlcomment(str)
+    # XML Comments start with <!-- and end with -->
+    #
+    # Because it's illegal for the first or last characters of a comment
+    # to be a -, i.e. <!--- or ---> are illegal, and also to improve
+    # readability, we add extra spaces after and before the beginning
+    # and end of comment markers.
+    #
+    # Also, XML Comments must not contain --. So we put a space between
+    # any double hyphens, by replacing any - that is followed by another -
+    # with '- '
+    data = ''
+    str.each_line do |line|
+      data << '<!-- ' << str.gsub(/-(?=-)/, '- ').chomp << " -->\n"
+    end
+    data
+  end
+
   cmd :secret do |cfg|
     cfg.gsub! /(\s+<bcrypt-hash>).+?(<\/bcrypt-hash>)/, '\\1<secret hidden>\\2'
     cfg.gsub! /(\s+<password>).+?(<\/password>)/, '\\1<secret hidden>\\2'
@@ -12,6 +30,13 @@ class PfSense < Oxidized::Model
     cfg.gsub! /\s<revision>\s*<time>\d*<\/time>\s*.*\s*.*\s*<\/revision>/, ''
     cfg.gsub! /\s<last_rule_upd_time>\d*<\/last_rule_upd_time>/, ''
     cfg
+  end
+
+  # The comment output has to be at the end since and XML file may not start
+  # with a comment.
+
+  cmd 'cat /etc/version' do |version|
+    xmlcomment "PFsense #{version}"
   end
 
   cfg :ssh do
