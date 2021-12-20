8a99505bb2ab73533fff0b72991251594c265b5b
Arturo Baldo <30608412+baldoarturo@users.noreply.github.com>
2021-12-20 14:46:48 -0300

    Update ufiber.rb
    Pass rubocop test

--- lib/oxidized/model/ufiber.rb
+++ lib/oxidized/model/ufiber.rb
@@ -1,18 +1,27 @@
+# UFiber #
 class UFiber < Oxidized::Model
-    
-    prompt /.+@.+:~\$/
-    
-    cmd 'cat /tmp/config'
 
-    cmd :all do |cfg|
-        cfg.lines.to_a[1..-2].join
-    end
-    
-    cfg :ssh do 
-        post_login "curl -s --output /dev/null --location --insecure --data 'username=#{@node.auth[:username]}&password=#{@node.auth[:password]}' --cookie-jar /tmp/cookies https://localhost"
-        post_login "curl -s --output /dev/null --location --insecure --cookie /tmp/cookies https://localhost/api/edge/config/save.json"
-        post_login "curl -s --location --insecure --cookie /tmp/cookies https://localhost/files/config/ | base64 > /tmp/config"
-        pre_logout 'exit'
-    end 
- 
+  prompt /.+@.+:~\$\s/
+
+  cmd 'cat /tmp/config'
+
+  cmd :all do |cfg|
+    cfg.lines.to_a[1..-2].join
+  end
+
+  cfg :ssh do
+    post_login "curl -s --output /dev/null --location --insecure '\
+                '--data '\
+                'username=#{@node.auth[:username]}&'\
+                'password=#{@node.auth[:password]} '\
+                '--cookie-jar /tmp/cookies https://localhost"
+    post_login 'curl -s --output /dev/null --location --insecure '\
+                '--cookie /tmp/cookies '\
+                'https://localhost/api/edge/config/save.json'
+    post_login 'curl -s --location --insecure '\
+                '--cookie /tmp/cookies '\
+                'https://localhost/files/config/ '\
+                '| base64 > /tmp/config'
+    pre_logout 'exit'
+  end
 end
