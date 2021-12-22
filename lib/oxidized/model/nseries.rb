fc443540404ba858ccd4db745f0a3cb02d06788f
Arturo Baldo <30608412+baldoarturo@users.noreply.github.com>
2021-12-22 11:41:34 -0300

    Support for Dell Networking N Series

---
+++ lib/oxidized/model/nseries.rb
@@ -0,0 +1,30 @@
+# Dell N Series
+class NSeries < Oxidized::Model
+	# Dell N Series
+
+	prompt /^([\w.@()-]+[#>]\s?)$/
+	comment '! '
+
+	cmd 'show version' do |cfg|
+		comment cfg
+	end
+
+	cmd 'show running-config'
+
+	cmd :all do |cfg|
+		cfg.lines.to_a[2..-2].join
+	end
+
+	cfg :telnet, :ssh do
+		if vars :enable
+			post_login do
+				send "enable\n"
+				cmd vars(:enable)
+			end
+		end
+		post_login 'terminal length 0'
+		pre_logout 'exit'
+		pre_logout 'exit'
+	end
+
+end
