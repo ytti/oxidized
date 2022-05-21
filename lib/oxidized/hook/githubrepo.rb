e891f5103d6ff2b8db1e38f40117d9e82e69273b
Mike Beattie <mike@ethernal.org>
2022-05-21 22:57:12 +1200

    Support group unique ssh keys for githubrepo hook (#2480)
    * Move creation of Rugged SshKey to function
    
    Allows optional usage of 'pubkey', derives from private key path (adds .pub)
    
    Signed-off-by: Mike Beattie <mike@ethernal.org>
    
    * Add support for configuring ssh key by privatekey only
    
    Signed-off-by: Mike Beattie <mike@ethernal.org>
    
    * Support a yaml dictionary for remote_repo groups
    
    pre-requisite for having group unique ssh keys, change repo url to a 'url'
    dictionary key
    
    Signed-off-by: Mike Beattie <mike@ethernal.org>
    
    * calculate credentials based on node
    
    Signed-off-by: Mike Beattie <mike@ethernal.org>
    
    * Support changing remote url
    
    Signed-off-by: Mike Beattie <mike@ethernal.org>
    
    * Make log content less wordy
    
    Signed-off-by: Mike Beattie <mike@ethernal.org>
    
    * Deal better with missing github repo for groups
    
    This also allows for only some repos to be pushed, if configured
    
    Signed-off-by: Mike Beattie <mike@ethernal.org>
    
    * Re-work ssh key config parsing
    
    Asetus doesn't support dig(), and private only vs public/private can be simplfied to a single check
    
    Signed-off-by: Mike Beattie <mike@ethernal.org>
    
    * Update documentation
    
    Signed-off-by: Mike Beattie <mike@ethernal.org>

--- lib/oxidized/hook/githubrepo.rb
+++ lib/oxidized/hook/githubrepo.rb
@@ -4,18 +4,32 @@ class GithubRepo < Oxidized::Hook
   end
 
   def run_hook(ctx)
-    repo = Rugged::Repository.new(ctx.node.repo)
+    repo  = Rugged::Repository.new(ctx.node.repo)
+    creds = credentials(ctx.node)
+    url   = remote_repo(ctx.node)
+
+    if url.nil? || url.empty?
+      log "No repository defined for #{ctx.node.group}/#{ctx.node.name}", :debug
+      return
+    end
+
     log "Pushing local repository(#{repo.path})..."
-    remote = repo.remotes['origin'] || repo.remotes.create('origin', remote_repo(ctx.node))
-    log "to remote: #{remote.url}"
+    log "to remote: #{url}"
 
-    fetch_and_merge_remote(repo)
+    if repo.remotes['origin'].nil?
+      repo.remotes.create('origin', url)
+    elsif repo.remotes['origin'].url != url
+      repo.remotes.set_url('origin', url)
+    end
+    remote = repo.remotes['origin']
 
-    remote.push([repo.head.name], credentials: credentials)
+    fetch_and_merge_remote(repo, creds)
+
+    remote.push([repo.head.name], credentials: creds)
   end
 
-  def fetch_and_merge_remote(repo)
-    result = repo.fetch('origin', [repo.head.name], credentials: credentials)
+  def fetch_and_merge_remote(repo, creds)
+    result = repo.fetch('origin', [repo.head.name], credentials: creds)
     log result.inspect, :debug
 
     unless result[:total_deltas].positive?
@@ -43,15 +57,20 @@ class GithubRepo < Oxidized::Hook
 
   private
 
-  def credentials
+  def credentials(node)
     Proc.new do |_url, username_from_url, _allowed_types| # rubocop:disable Style/Proc
       git_user = cfg.has_key?('username') ? cfg.username : (username_from_url || 'git')
       if cfg.has_key?('password')
         log "Authenticating using username and password as '#{git_user}'", :debug
         Rugged::Credentials::UserPassword.new(username: git_user, password: cfg.password)
-      elsif cfg.has_key?('publickey') && cfg.has_key?('privatekey')
+      elsif cfg.has_key?('privatekey')
+        pubkey = cfg.has_key?('publickey') ? cfg.publickey : nil;
         log "Authenticating using ssh keys as '#{git_user}'", :debug
-        Rugged::Credentials::SshKey.new(username: git_user, publickey: File.expand_path(cfg.publickey), privatekey: File.expand_path(cfg.privatekey), passphrase: ENV["OXIDIZED_SSH_PASSPHRASE"])
+        rugged_sshkey(git_user: git_user, privkey: cfg.privatekey, pubkey: pubkey)
+      elsif cfg.has_key?('remote_repo') && cfg.remote_repo.has_key?(node.group) && cfg.remote_repo[node.group].has_key?('privatekey')
+        pubkey = cfg.remote_repo[node.group].has_key?('publickey') ? cfg.remote_repo[node.group].publickey : nil;
+        log "Authenticating using ssh keys as '#{git_user}' for '#{node.group}/#{node.name}'", :debug
+        rugged_sshkey(git_user: git_user, privkey: cfg.remote_repo[node.group].privatekey, pubkey: pubkey)
       else
         log "Authenticating using ssh agent as '#{git_user}'", :debug
         Rugged::Credentials::SshKeyFromAgent.new(username: git_user)
@@ -59,11 +78,25 @@ class GithubRepo < Oxidized::Hook
     end
   end
 
+  def rugged_sshkey(args = {})
+    git_user   = args[:git_user]
+    privkey    = args[:privkey]
+    pubkey     = args[:pubkey] || privkey + '.pub'
+    Rugged::Credentials::SshKey.new(username: git_user,
+                                    publickey: File.expand_path(pubkey),
+                                    privatekey: File.expand_path(privkey),
+                                    passphrase: ENV["OXIDIZED_SSH_PASSPHRASE"])
+  end
+
   def remote_repo(node)
     if node.group.nil? || cfg.remote_repo.is_a?(String)
       cfg.remote_repo
-    else
+    elsif cfg.remote_repo[node.group].is_a?(String)
       cfg.remote_repo[node.group]
+    elsif cfg.remote_repo[node.group].url.is_a?(String)
+      cfg.remote_repo[node.group].url
+    else
+      nil
     end
   end
 end
