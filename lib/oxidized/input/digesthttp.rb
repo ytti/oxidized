8744691cd96bb59af3cc4a32a9a5f4f1a649e155
rallakwash <varrotomi@gmail.com>
2022-07-07 21:19:03 +0200

    Digest HTTP auth
    Added basic support for digest HTTP auth

---
+++ lib/oxidized/input/digesthttp.rb
@@ -0,0 +1,89 @@
+module Oxidized
+  require "oxidized/input/cli"
+  require "net/http"
+  require "net/http/digest_auth"
+  require "json"
+
+  class DIGESTHTTP < Input
+    include Input::CLI
+
+    def connect(node)
+      @node = node
+      @secure = false
+      @username = nil
+      @password = nil
+      @headers = {}
+      @log = File.open(Oxidized::Config::Log + "/#{@node.ip}-http", "w") if Oxidized.config.input.debug?
+      @node.model.cfg["http"].each { |cb| instance_exec(&cb) }
+
+      return true unless @main_page && defined?(login)
+
+      begin
+        require "mechanize"
+      rescue LoadError
+        raise OxidizedError, "mechanize not found: sudo gem install mechanize"
+      end
+
+      @m = Mechanize.new
+      url = URI::HTTP.build host: @node.ip, path: @main_page
+      @m_page = @m.get(url.to_s)
+      login
+    end
+
+    def cmd(callback_or_string)
+      return cmd_cb callback_or_string if callback_or_string.is_a?(Proc)
+
+      cmd_str callback_or_string
+    end
+
+    def cmd_cb(callback)
+      instance_exec(&callback)
+    end
+
+    def cmd_str(string)
+      path = string % { password: @node.auth[:password] }
+      get_http path
+    end
+
+    private
+
+    def get_http(path)
+      schema = @secure ? "https://" : "http://"
+      uri = URI("#{schema}#{@node.ip}#{path}")
+      req = Net::HTTP::Get.new(uri)
+      uri.user = @node.auth[:username]
+      uri.password = @node.auth[:password]
+
+      h = Net::HTTP.new uri.host, uri.port
+
+      req = Net::HTTP::Get.new uri.request_uri
+
+      res = h.request req
+
+      digest_auth = Net::HTTP::DigestAuth.new
+      auth = digest_auth.auth_header uri, res['www-authenticate'], 'GET'
+
+      req = Net::HTTP::Get.new uri.request_uri
+      req.add_field 'Authorization', auth
+
+      res = h.request req
+
+      @headers.each do |header, value|
+        req.add_field(header, value)
+      end
+      ssl_verify = Oxidized.config.input.http.ssl_verify? ? OpenSSL::SSL::VERIFY_PEER : OpenSSL::SSL::VERIFY_NONE
+      res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https", verify_mode: ssl_verify) do |http|
+        http.request(req)
+      end
+      res.body
+    end
+
+    def log(str)
+      @log&.write(str)
+    end
+
+    def disconnect
+      @log.close if Oxidized.config.input.debug?
+    end
+  end
+end
