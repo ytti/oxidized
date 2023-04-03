a1a159d72b938f4cd966d12b13d0971abaa3ab23
Alexander Schaber <a.schaber@cuegee.com>
2023-04-03 08:10:04 +0200

    ci: update ci pipeline to new codecov action

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,8 @@
 require 'simplecov'
 SimpleCov.start
 
-if ENV['CI'] == 'true'
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
-end
+require 'simplecov-cobertura'
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
 
 require 'minitest/autorun'
 require 'mocha/minitest'
