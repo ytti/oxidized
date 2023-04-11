409f53e71364288c70debe2d50d0be1c55897bdf
Alexander Schaber <a.schaber@cuegee.com>
2023-04-11 14:49:38 +0200

    fix(Oxidized::String): fails when strings are empty + tests

--- lib/oxidized/string.rb
+++ lib/oxidized/string.rb
@@ -5,16 +5,22 @@ module Oxidized
 
     # @return [Oxidized::String] copy of self with last line removed
     def cut_tail(lines = 1)
+      return Oxidized::String.new("") if length.zero?
+
       Oxidized::String.new each_line.to_a[0..(-1 - lines)].join
     end
 
     # @return [Oxidized::String] copy of self with first line removed
     def cut_head(lines = 1)
+      return Oxidized::String.new("") if length.zero?
+
       Oxidized::String.new each_line.to_a[lines..-1].join
     end
 
     # @return [Oxidized::String] copy of self with first and last lines removed
     def cut_both(head = 1, tail = 1)
+      return Oxidized::String.new("") if length.zero?
+
       Oxidized::String.new each_line.to_a[head..(-1 - tail)].join
     end
 
