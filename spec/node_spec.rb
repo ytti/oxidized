0ac9bbf52ca47a7db49acf01c765e1a0b79f0370
Alexander Schaber <a.schaber@cuegee.com>
2023-04-14 10:42:26 +0200

    test: use require_relative for `spec_helper`

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require_relative 'spec_helper'
 
 describe Oxidized::Node do
   before(:each) do
--- spec/string_spec.rb
+++ spec/string_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require_relative 'spec_helper'
 require 'oxidized/string'
 
 describe Oxidized::String do
