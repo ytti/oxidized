0eb1abdd0d3266ef0d434ddee1484f385d3090f5
Dave <davama@gmail.com>
2023-06-29 16:37:26 -0400

    update http.rb to check for config settings

--- lib/oxidized/source/http.rb
+++ lib/oxidized/source/http.rb
@@ -83,20 +83,24 @@ module Oxidized
 
       node_data = []
 
-      something = true
-      if not something
+      # since new feature; dont break curent configs
+      if not @cfg.pagination?
         data = JSON.parse(response.body)
         node_data += string_navigate(data, @cfg.hosts_location) if @cfg.hosts_location?
         return node_data
       else
+	if not @cfg.pagination_key_name?
+          raise Oxidized::OxidizedError, "if using pagination, 'pagination_key_name' setting must be set"
+	end
+	next_key = @cfg.pagination_key_name
         loop do
           data = JSON.parse(response.body)
           node_data += string_navigate(data, @cfg.hosts_location) if @cfg.hosts_location?
-          if data['next'].nil?
+          if data[next_key].nil?
             break
           end
-          if data.key?('next')
-            new_uri = URI.parse(data['next'])
+          if data.key?(next_key)
+            new_uri = URI.parse(data[next_key])
           end
           request = set_request(new_uri, headers, node_want)
           response = http.request(request)
