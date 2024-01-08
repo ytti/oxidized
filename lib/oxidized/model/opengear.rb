66a6cc33f8aa9a5998adde5527e0f19bb5a05c02
Matej Vadnjal <matej.vadnjal@arnes.si>
2024-01-08 12:10:52 +0100

    opengear: add support for newer firmware versions

--- lib/oxidized/model/opengear.rb
+++ lib/oxidized/model/opengear.rb
@@ -8,22 +8,51 @@ class OpenGear < Oxidized::Model
   cmd :secret do |cfg|
     cfg.gsub!(/password (\S+)/, 'password <secret removed>')
     cfg.gsub!(/community (\S+)/, 'community <secret removed>')
+    cfg.gsub!(/community=(\S+)/, 'community=<secret removed>')
+    cfg.gsub!(/private_key=(\S+)/, 'private_key=<secret removed>')
+    cfg.gsub!(/ key=(\S+)/, ' key=<secret removed>')
+    cfg.gsub!(/hashed_password=(\S+)/, 'hashed_password=<secret removed>')
     cfg
   end
 
-  cmd('cat /etc/version') { |cfg| comment cfg }
+  cmd 'cat /etc/version' do |cfg|
+      cfg.gsub! /^/, 'OS Version: '
+      comment cfg
+  end
 
+  # newer opengear firmware versions
+  cmd 'ogdeviceinfo -r' do |cfg|
+    if not cfg.include? "ogdeviceinfo: command not found"
+      comment cfg
+    end
+  end
+
+  cmd 'config export' do |cfg|
+    if not cfg.include? "usage: config"
+      out = ''
+      cfg.each_line do |line|
+        out << line
+      end
+      out
+    end
+  end
+
+  # older opengear firmware versions
   cmd 'showserial' do |cfg|
-    cfg.gsub! /^/, 'Serial Number: '
-    comment cfg
+    if not cfg.include? "showserial: command not found"
+      cfg.gsub! /^/, 'Serial Number: '
+      comment cfg
+    end
   end
 
   cmd 'config -g config' do |cfg|
-    out = ''
-    cfg.each_line do |line|
-      out << line
+    if not cfg.include? "config: error: argument"
+      out = ''
+      cfg.each_line do |line|
+        out << line
+      end
+      out
     end
-    out
   end
 
   cfg :ssh do
