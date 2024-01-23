ad4a829940064bcb44f600f01374abbee66512a3
Brian Candler <b.candler@pobox.com>
2024-01-23 14:56:22 +0800

    Remove "<timestamp> by RouterOS" comment lines from ROS v7
    Fixes #3021
    
    Signed-off-by: Brian Candler <b.candler@pobox.com>

--- lib/oxidized/model/routeros.rb
+++ lib/oxidized/model/routeros.rb
@@ -45,7 +45,9 @@ class RouterOS < Oxidized::Model
       cfg.gsub! "# poe-out status: short_circuit\r\n", '' # Remove intermittent POE short_circuit comment
       cfg.gsub! "# Firmware upgraded successfully, please reboot for changes to take effect!\r\n", '' # Remove transient firmware upgrade comment
       cfg.gsub! /# \S+ not ready\r\n/, '' # Remove intermittent $interface not ready comment
-      cfg = cfg.split("\n").reject { |line| line[/^#\s\w{3}\/\d{2}\/\d{4}\s\d{2}:\d{2}:\d{2}.*$/] || line[/^#\s\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.*$/] } # Remove date time and 'by RouterOS' comment
+      cfg = cfg.split("\n")
+      cfg.reject! { |line| line[/^#\s\w{3}\/\d{2}\/\d{4}.*$/] } # Remove date time and 'by RouterOS' comment (v6)
+      cfg.reject! { |line| line[/^#\s\d{4}-\d{2}-\d{2}.*$/] }   # Remove date time and 'by RouterOS' comment (v7)
       cfg.join("\n") + "\n"
     end
   end
