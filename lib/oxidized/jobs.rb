ytti/oxidized

commit 7bfc6ca1e1986f6f97f84cbd1f6eb319fb734720

Prevent potential busy loop in scheduler from jobs > nodes (#3060)
* Prevent potential busy loop in scheduler

This change remediates a potential busy-loop in the scheduler which
results from either next_adds_job allowing the job count to exceed the node count
or a retry after configuration fetch failure causing the same invalid state.

* Update changelog

* Summarize / simplify the job class increment function

lib/oxidized/jobs.rb
@@ -44,6 +44,14 @@ module Oxidized
       @want = @max if @want > @max
     end
 
+    def increment
+      # Increments the job count if safe to do so, which means:
+      # a) less threads running than the total amount of nodes
+      # b) we want less than the max specified number of threads
+
+      want = [(@want + 1), @nodes.size, @max].min
+    end
+
     def work
       # if   a) we want less or same amount of threads as we now running
       # and  b) we want less threads running than the total amount of nodes
@@ -51,7 +59,9 @@ module Oxidized
       # then we want one more thread (rationale is to fix hanging thread causing HOLB)
       return unless @want <= size && @want < @nodes.size
 
-      @want += 1 if (Time.now.utc - @last) > MAX_INTER_JOB_GAP
+      return unless @want <= size
+
+      increment if (Time.now.utc - @last) > MAX_INTER_JOB_GAP
     end
   end
 end

lib/oxidized/nodes.rb
@@ -80,7 +80,7 @@ module Oxidized
         # set last job to nil so that the node is picked for immediate update
         n.last = nil
         put n
-        jobs.want += 1 if Oxidized.config.next_adds_job?
+        jobs.increment if Oxidized.config.next_adds_job?
       end
     end
     alias top next

commit ab202accd64af68a8076ee9856a19b73823c9c28

Correct accidental regression in scheduler introduced in https://github.com/ytti/oxidized/pull/3060

lib/oxidized/jobs.rb
@@ -49,7 +49,7 @@ module Oxidized
       # a) less threads running than the total amount of nodes
       # b) we want less than the max specified number of threads
 
-      want = [(@want + 1), @nodes.size, @max].min
+      @want = [(@want + 1), @nodes.size, @max].min
     end
 
     def work