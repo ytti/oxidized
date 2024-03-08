3bc768ee3c84800f397608fa6a9f4f26aa971c13
Angel <angel@beanfield.com>
2024-03-08 13:28:46 -0500

    Initial implementation of the Fujistu 1Finity

---
+++ lib/oxidized/model/onefinity.rb
@@ -0,0 +1,19 @@
+class OneFinity < Oxidized::Model
+  using Refinements
+
+  # Fujitsu 1finity
+
+  prompt /(\r?[\w.@_()-]+[>]\s?)$/
+
+  cmd :all do |cfg|
+    cfg.each_line.to_a[1..-3].join
+  end
+
+  cmd 'show configuration | display set | nomore' do |cfg|
+    cfg
+  end
+
+  cfg :ssh do
+    pre_logout 'exit'
+  end
+end
