6da869a29d0dae4bf1350734dd55131d14d2af92
Neil Lathwood <gh+n@laf.io>
2024-09-17 09:24:57 +0100

    Updated tests

--- spec/source/http_spec.rb
+++ spec/source/http_spec.rb
@@ -63,16 +63,16 @@ describe Oxidized::HTTP do
       _(http.send(:string_navigate_object, h1, "jotain.2")).must_equal "jotain"
     end
     it "should be able to navigate multilevel-hash" do
-      _(Oxidized::HTTP.new.send(:string_navigate_object, h1, "jotain.2")).must_equal "jotain"
+      _(Oxidized::Source::HTTP.new.send(:string_navigate_object, h1, "jotain.2")).must_equal "jotain"
     end
     it "should be able to navigate hash/array combination" do
-      _(Oxidized::HTTP.new.send(:string_navigate_object, h1, "inventory[0].ip")).must_equal "10.10.10.10"
+      _(Oxidized::Source::HTTP.new.send(:string_navigate_object, h1, "inventory[0].ip")).must_equal "10.10.10.10"
     end
     it "should return nil on non-existing string key" do
-      _(Oxidized::HTTP.new.send(:string_navigate_object, h1, "jotain.3")).must_be_nil
+      _(Oxidized::Source::HTTP.new.send(:string_navigate_object, h1, "jotain.3")).must_be_nil
     end
     it "should return nil on non-existing array index" do
-      _(Oxidized::HTTP.new.send(:string_navigate_object, h1, "inventory[3]")).must_be_nil
+      _(Oxidized::Source::HTTP.new.send(:string_navigate_object, h1, "inventory[3]")).must_be_nil
     end
   end
 end
