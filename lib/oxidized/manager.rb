46497aac45cdaea1cefbb3098ab6ff0ff2d331dc
Neil Lathwood <gh+n@laf.io>
2024-10-09 21:40:58 +0100

    Updated source files to avoid namespace duplication

--- lib/oxidized/manager.rb
+++ lib/oxidized/manager.rb
@@ -8,7 +8,7 @@ module Oxidized
       def load(dir, file)
         require File.join dir, file + '.rb'
         klass = nil
-        [Oxidized, Object].each do |mod|
+        [Oxidized, Source, Object].each do |mod|
           klass   = mod.constants.find { |const| const.to_s.casecmp(file).zero? }
           klass ||= mod.constants.find { |const| const.to_s.downcase == 'oxidized' + file.downcase }
           klass   = mod.const_get klass if klass
