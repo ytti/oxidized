9f9201d332c38cca38cf93c4e167207114cfecbe
Robert Cheramy <r.cheramy@netze-bw.de>
2024-11-14 05:24:43 +0100

    Create Unit-Test for 6410_FL10,10,1100
    I also renamed the YAML simulation file and made the password ciphertext
    explicitly wrong, placeing AAAAAAAAABBBBBBBBBBCCCCCCCC into it.

--- spec/model/aoscx_spec.rb
+++ spec/model/aoscx_spec.rb
@@ -21,4 +21,14 @@ describe 'model/Aoscx' do
     _(status).must_equal :success
     _(result.to_cfg).must_equal mockmodel.oxidized_output
   end
+
+  it 'runs on R8N85A (C6000-48G-CL4) with PL.10.08.1010' do
+    mockmodel = MockSsh.new('examples/device-simulation/yaml/aoscx_R0X25A-6410_FL.10.10.1100.yaml')
+    Net::SSH.stubs(:start).returns mockmodel
+
+    status, result = @node.run
+
+    _(status).must_equal :success
+    _(result.to_cfg).must_equal mockmodel.oxidized_output
+  end
 end
