6a0dd6bdf43ce5546ec70a1c0e284f732314fc26
Morgan Pilmark <128702223+mopi3456@users.noreply.github.com>
2024-12-04 09:08:50 +0100

    Update gitdiff-msteams.sh
    Updated with button that links to internal github

--- extra/gitdiff-msteams.sh
+++ extra/gitdiff-msteams.sh
@@ -17,7 +17,12 @@
 # Add webhook to your MS Teams channel and set the next variable to the full url
 
 weburl="https://contoso.webhook.office.com/webhookb2/etc etc etc"
-NEWLINE=$'\n'
+GITURL="https://github.example.com/My-org/oxidized/commit/"
+
+# Max size before shortening
+MAXSIZE=20000
+# When shortening - how many lines to show
+SHORTLINES=30
 
 postdata()
 {
@@ -83,8 +88,8 @@ postdata()
                     "actions": [
                         {
                         "type": "Action.OpenUrl",
-                        "title": "Klik her for visning",
-                        "url": "https://internalgithub/org/oxidized/commit/${OX_REPO_COMMITREF}"
+                        "title": "Click to see diff in github",
+                        "url": "${GITURL}${OX_REPO_COMMITREF}"
                     }
                 ]
             }
@@ -97,11 +102,12 @@ EOF
 }
 
 COMMIT=$(git --bare --git-dir="${OX_REPO_NAME}" show --pretty='' --no-color "${OX_REPO_COMMITREF}" | jq --raw-input --slurp --compact-output)
+URL=""
 
 size=$(postdata | wc -c)
-if [ "$size" -gt "15000" ]; then
-  COMMIT=$(git --bare --git-dir="${OX_REPO_NAME}" show --pretty='' --no-color "${OX_REPO_COMMITREF}" | head -n 20)
-  COMMIT+="$NEWLINE...$NEWLINE Shortened because of length"
+if [ "$size" -gt "$MAXSIZE" ]; then
+  COMMIT=$(git --bare --git-dir="${OX_REPO_NAME}" show --pretty='' --no-color "${OX_REPO_COMMITREF}" | head -n $SHORTLINES)
+  COMMIT+="$NEWLINE...$NEWLINE Shortened because of length, see full diff by clicking below button"
   COMMIT=$(echo "${COMMIT}" | jq --raw-input --slurp --compact-output )
 fi
 
