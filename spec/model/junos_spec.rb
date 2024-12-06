e0d37986ad04027e8819dce8a94dc357de8f2184
Bert Roelofsen <19684880+systeembeheerder@users.noreply.github.com>
2024-12-06 13:01:52 +0100

    add junos model unit test

---
+++ spec/model/junos_spec.rb
@@ -0,0 +1,21 @@
+require_relative 'model_helper'
+
+describe 'model/junos' do
+  before(:each) do
+    init_model_helper
+    @node = Oxidized::Node.new(name:  'example.com',
+                               input: 'ssh',
+                               model: 'junos')
+  end
+
+  it 'runs on SRX300 with 22.4' do
+    mockmodel = MockSsh.new('examples/device-simulation/yaml/junos_srx300_22.4.yaml')
+    Net::SSH.stubs(:start).returns mockmodel
+
+    status, result = @node.run
+
+    _(status).must_equal :success
+    result2file(result, 'model-output.txt')
+    _(result.to_cfg).must_equal mockmodel.oxidized_output
+  end
+end
