4671a07b103f571ce2dc7542595e7b01fb90953c
Saku Ytti <saku@ytti.fi>
2024-12-21 10:58:53 +0200

    Refactor ATOMS
    Flat list of tests instead of hash of tests with type key

--- spec/model/model_atoms_spec.rb
+++ spec/model/model_atoms_spec.rb
@@ -6,30 +6,28 @@ require_relative 'model_helper'
 class ATOMS
   DIRECTORY = 'spec/model/data'.freeze
   def self.tests_get
-    tests = {
-      output: [],
-      prompt: []
-    }
+    tests = []
 
     Dir[File.join(DIRECTORY, '*:output.txt')].each do |file|
-      model, desc, _type = *File.basename(file, '.txt').split(':')
-      tests[:output] << TestOutput.new(model, desc)
+      model, desc, type = *File.basename(file, '.txt').split(':')
+      tests << TestOutput.new(model, desc, type)
     end
 
     Dir[File.join(DIRECTORY, '*:prompt.yaml')].each do |file|
-      model, desc, _type = *File.basename(file, '.yaml').split(':')
-      tests[:prompt] << TestPrompt.new(model, desc)
+      model, desc, type = *File.basename(file, '.yaml').split(':')
+      tests << TestPrompt.new(model, desc, type)
     end
 
     tests
   end
 
   class Test
-    attr_reader :model, :desc
+    attr_reader :model, :desc, :type
 
-    def initialize(model, desc)
+    def initialize(model, desc, type)
       @model = model
       @desc = desc
+      @type = type
       @skip = false
     end
 
@@ -42,7 +40,8 @@ class ATOMS
       [@model, @desc, type].join(':') + ext
     end
 
-    def load_file(file_name)
+    def load_file(type = nil)
+      file_name = get_filename((type or @type))
       ext = File.extname(file_name)
       if ext == '.yaml'
         YAML.load_file(File.join(DIRECTORY, file_name))
@@ -57,11 +56,11 @@ class ATOMS
   class TestOutput < Test
     attr_reader :simulation, :output
 
-    def initialize(model, desc)
+    def initialize(model, desc, type)
       super
 
-      @simulation = load_file(get_filename('simulation'))
-      @output = load_file(get_filename('output'))
+      @simulation = load_file('simulation')
+      @output = load_file
       @skip = true unless @simulation && @output
     end
   end
@@ -69,10 +68,10 @@ class ATOMS
   class TestPrompt < Test
     attr_reader :data
 
-    def initialize(model, desc)
+    def initialize(model, desc, type)
       super
 
-      @data = load_file(get_filename('prompt'))
+      @data = load_file
       @skip = true unless @data
     end
 
@@ -87,37 +86,35 @@ class ATOMS
 end
 
 describe 'ATOMS tests' do
-  ATOMS.tests_get.each do |type, tests|
-    tests.each do |test|
-      test_string = "(#{test.model} / #{test.desc})"
+  ATOMS.tests_get.each do |test|
+    test_string = "ATOMS/#{test.type} (#{test.model} / #{test.desc})"
 
-      before(:each) do
-        init_model_helper
-        @node = Oxidized::Node.new(name:  'example.com',
-                                   input: 'ssh',
-                                   model: test.model)
+    before(:each) do
+      init_model_helper
+      @node = Oxidized::Node.new(name:  'example.com',
+                                 input: 'ssh',
+                                 model: test.model)
+    end
+
+    if test.type == 'output'
+      it "#{test_string} has expected output" do
+        skip("check simulation+output data file for #{test_string}") if test.skip?
+        mockmodel = MockSsh2.new(test)
+        Net::SSH.stubs(:start).returns mockmodel
+        status, result = @node.run
+        _(status).must_equal :success
+        _(result.to_cfg).must_equal mockmodel.oxidized_output
       end
 
-      if type == :output
-        it "ATOMS #{test_string} has expected output" do
-          skip("check simulation+output data file for #{test_string}") if test.skip?
-          mockmodel = MockSsh2.new(test)
-          Net::SSH.stubs(:start).returns mockmodel
-          status, result = @node.run
-          _(status).must_equal :success
-          _(result.to_cfg).must_equal mockmodel.oxidized_output
+    elsif test.type == 'prompt'
+      it "#{test_string} has working prompt detection" do
+        skip("check prompt data file for #{test_string}") if test.skip?
+        prompt_re = Object.const_get(test.model.upcase).prompt
+        test.pass.each do |want_pass|
+          _(want_pass).must_match prompt_re
         end
-
-      elsif type == :prompt
-        it "ATOMS #{test_string} has working prompt detection" do
-          skip("check prompt data file for #{test_string}") if test.skip?
-          prompt_re = Object.const_get(test.model.upcase).prompt
-          test.pass.each do |want_pass|
-            _(want_pass).must_match prompt_re
-          end
-          test.fail.each do |want_fail|
-            _(want_fail).wont_match prompt_re
-          end
+        test.fail.each do |want_fail|
+          _(want_fail).wont_match prompt_re
         end
       end
     end
