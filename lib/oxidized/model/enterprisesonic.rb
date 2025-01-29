f221d86bf40be4bb6b5eaa2d140db27a1ac96c59
Ilmari Aho <ilmari.aho@upcloud.com>
2025-01-29 16:00:23 +0200

    add Enterprise SONiC support

---
+++ lib/oxidized/model/enterprisesonic.rb
@@ -0,0 +1,25 @@
+class EnterpriseSonic < Oxidized::Model
+  using Refinements
+
+  # Remove ANSI escape codes
+  expect /\e\[[0-?]*[ -\/]*[@-~]\r?/ do |data, re|
+    data.gsub re, ''
+  end
+
+  prompt /^(\w.*|\W.*)[:#$] /
+  comment "# "
+
+  cmd 'show running-configuration | no-more' do |cfg|
+    comment cfg
+  end
+
+  cmd 'show version | no-more' do |cfg|
+    cfg = cfg.each_line.grep(/Software Version|Product|Distribution|Kernel|Config DB Version|Build Commit|Platform|HwSKU|ASIC|Hardware Version|Serial Number|Mfg/).join
+    comment cfg
+  end
+
+  cfg :ssh do
+    post_login 'sonic-cli'
+    pre_logout 'exit'
+  end
+end
