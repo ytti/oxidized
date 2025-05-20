53699010f11430d5b5bf6c0f094da6dddad3a9fe
Tomáš Holý <vantomas@gmail.com>
2025-05-20 22:48:05 +0200

    tnsr: prompt regexp optimized

--- lib/oxidized/model/tnsr.rb
+++ lib/oxidized/model/tnsr.rb
@@ -3,7 +3,20 @@ class TNSR < Oxidized::Model
 
   # Netgate TNSR #
 
-  prompt /^.+[#]\s$/
+  # prompt examples
+  # https://docs.netgate.com/tnsr/en/latest/basics/working-cli.html#command-prompt
+  #  "<hostname> tnsr<(mode)># " <-- with trailing whitespace
+  #  tnsr-dev-25-02 tnsr#
+  #  tnsr-dev-25-02 tnsr(config)#  <-- after "configure terminal", but this is not important for oxidized
+  # prompt from debug log
+  #  received "Welcome to Netgate TNSR Version: 25.02-2\r\n\r\n\t* Documentation:  https://docs.netgate.com/tnsr/en/latest/\r\n\t* Support:        https://www.netgate.com/support\r\n\r\n   _          __________\r\n _| |_ _ __  | ___ _ __ |\r\n|_  __| '_ \\ |/ __| '__||\r\n  | |_| | | ||\\__ \\ |   |\r\n   \\__|_| |_|||___/_|   |\r\n             |__________|\r\n\r\n\r\n"
+  #  received "\r\nVersion: 25.02-2\r\n\r\nFor information see 'show documentation'\r\n\r\n"
+  #  received "\rbgp01 tnsr# "
+  # when the --More-- pager is the last line of paged output, the prompt contains \x08\x20\x08 orphans on the begin
+  #  you can look for context into spec simulation:
+  #    spec/model/data/tnsr:TNSR_24.10-3_short-config:simulation.yaml
+  #    spec/model/data/tnsr:TNSR_25.02-2_long-config-and-pager-at-last-line:simulation.yaml
+  prompt /^((\x08{8}\x20{8}\x08{8})?\r?[\w-]+\stnsr#\s?)$/
 
   comment '! '
 
