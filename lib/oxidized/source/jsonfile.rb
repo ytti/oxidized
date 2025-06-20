998d3675b58502bb32c3e3ad95d2f0f08267ff94
Saku Ytti <saku@ytti.fi>
2025-06-20 09:08:06 +0300

    Normalise vars keys to strings, not symbols
    I prefer symbol heres, but because these vars can come from source
    or form config it probably makes sense to have all of them strings.
    
    Changing config to symbols would be inconvenient without adding
    feature to Asetus library to transform keys.

--- lib/oxidized/source/jsonfile.rb
+++ lib/oxidized/source/jsonfile.rb
@@ -50,7 +50,7 @@ module Oxidized
           # map node specific vars
           vars = {}
           @cfg.vars_map.each do |key, want_position|
-            vars[key.to_sym] = node_var_interpolate string_navigate_object(node, want_position)
+            vars[key.to_s] = node_var_interpolate string_navigate_object(node, want_position)
           end
           keys[:vars] = vars unless vars.empty?
 
