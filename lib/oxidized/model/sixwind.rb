c6576b72e4985e553e6a234365e2c3e7b27b28a2
Harrison Caldicott <harrison@itsfubar.com.au>
2025-06-26 23:02:12 +1000

    sixwind.rb rubocop corrections

--- lib/oxidized/model/sixwind.rb
+++ lib/oxidized/model/sixwind.rb
@@ -3,20 +3,20 @@ class SixWind < Oxidized::Model
 
   prompt /^[\w\s\(\).@_\/:-]+[>] $/
   comment '# '
-  
+
   cmd :all do |cfg|
     cfg.cut_both
   end
-  
+
   cmd :secret do |cfg|
     cfg.gsub!(/(?<!  {1})(?:password|secret) (?:\d )?\S+/, '\\1 <secret hidden>') # double space to exclude radius password template
     cfg
   end
-  
+
   cmd 'show product version' do |cfg|
     comment cfg
   end
- 
+
   cmd 'show config' do |cfg|
     cfg
   end
