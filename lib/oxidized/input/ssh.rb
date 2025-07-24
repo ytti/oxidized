9694cff0ccb868cdd661a7bf644da385a1780bbd
Saku Ytti <saku@ytti.fi>
2025-07-24 09:51:24 +0300

    input/ssh: fix typo

--- lib/oxidized/input/ssh.rb
+++ lib/oxidized/input/ssh.rb
@@ -81,7 +81,7 @@ module Oxidized
       Timeout.timeout(Oxidized.config.timeout) { @ssh.loop }
     rescue Errno::ECONNRESET, Net::SSH::Disconnect, IOError => e
       logger.debug 'The other side closed the connection while ' \
-                   "disconnecting, rasing #{e.class} with #{e.message}"
+                   "disconnecting, raising #{e.class} with #{e.message}"
     rescue Timeout::Error
       logger.debug "#{@node.name} timed out while disconnecting"
     ensure
