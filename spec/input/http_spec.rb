e04d8eff636f0e47a6f3319911cee5f6c3f9f86b
Saku Ytti <saku@ytti.fi>
2025-08-23 12:04:45 +0300

    Move methods under describe
    Otherwise we carry state and affect other tests

--- spec/input/http_spec.rb
+++ spec/input/http_spec.rb
@@ -1,29 +1,26 @@
 require_relative '../spec_helper'
 require 'oxidized/input/http'
 
-def get_node(ip = "127.0.0.1")
-  Oxidized::Node.new(ip:       ip,
-                     name:     'example.com',
-                     input:    'http',
-                     output:   'git',
-                     model:    'junos',
-                     username: 'user',
-                     password: 'pass')
-end
-
-def get_uri(ip, path, secure: false)
-  @http = Oxidized::HTTP.new
-  @http.connect(get_node(ip))
-  @http.instance_variable_set("@secure", secure)
-  @http.send("get_uri", path)
-end
-
 describe Oxidized::HTTP do
+  def get_node(ip = "127.0.0.1")
+    Oxidized::Node.new(ip:       ip,
+                       name:     'example.com',
+                       input:    'http',
+                       output:   'git',
+                       model:    'junos',
+                       username: 'user',
+                       password: 'pass')
+  end
+
+  def get_uri(ip, path, secure: false)
+    @http = Oxidized::HTTP.new
+    @http.connect(get_node(ip))
+    @http.instance_variable_set("@secure", secure)
+    @http.send("get_uri", path)
+  end
+
   before(:each) do
     Oxidized.asetus = Asetus.new
-    Oxidized::Node.any_instance.stubs(:resolve_repo)
-    Oxidized::Node.any_instance.stubs(:resolve_input)
-    Oxidized::Node.any_instance.stubs(:resolve_output)
   end
 
   describe "#connect" do
