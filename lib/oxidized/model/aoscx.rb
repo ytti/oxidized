069f4b0306eeecdfcc411af45cd8bc4ec496e844
Robert Cheramy <r.cheramy@netze-bw.de>
2025-10-26 07:43:19 +0100

    aoscx: rework handling of ANSI escape codes

--- lib/oxidized/model/aoscx.rb
+++ lib/oxidized/model/aoscx.rb
@@ -1,25 +1,13 @@
 class Aoscx < Oxidized::Model
   using Refinements
+  # HPE Aruba Networking - ArubaOS-CX models
 
-  # previous command is repeated followed by "\eE", which sometimes ends up on last line
-  # ssh switches prompt may start with \r, followed by the prompt itself, regex ([\w\s.-]+[#>] ), which ends the line
-  # telnet switches may start with various vt100 control characters, regex (\e\[24;[0-9][hH]),
-  # followed by the prompt, followed by at least 3 other vt100 characters
-  prompt /(^\r|\e\[24;[0-9][hH])?([\w\s.-]+[#>] )($|(\e\[24;[0-9][0-9]?[hH]){3})/
+  prompt /^[\w\s.-]+[#>] $/
+  clean :escape_codes
 
   comment '! '
 
-  # replace next line control sequence with a new line
-  expect /(\e\[1M\e\[\??\d+(;\d+)*[A-Za-z]\e\[1L)|(\eE)/ do |data, re|
-    data.gsub re, "\n"
-  end
-
-  # replace all used vt100 control sequences
-  expect /\e\[\??\d+(;\d+)*[A-Za-z]/ do |data, re|
-    data.gsub re, ''
-  end
-
-  expect /Press any key to continue(\e\[\??\d+(;\d+)*[A-Za-z])*$/ do
+  expect /Press any key to continue$/ do
     send ' '
     ""
   end
@@ -30,13 +18,7 @@ class Aoscx < Oxidized::Model
   end
 
   cmd :all do |cfg|
-    cfg = cfg.cut_both
-    cfg = cfg.gsub /^\r/, ''
-    # Additional filtering for elder switches sending vt100 control chars via telnet
-    cfg.gsub! /\e\[\??\d+(;\d+)*[A-Za-z]/, ''
-    # Additional filtering for power usage reporting which obviously changes over time
-    cfg.gsub! /^(.*AC [0-9]{3}V\/?([0-9]{3}V)?) *([0-9]{1,3}) (.*)/, '\\1 <removed> \\4'
-    cfg
+    cfg.cut_both
   end
 
   cmd :secret do |cfg|
