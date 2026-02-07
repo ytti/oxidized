b4bbca5f1787ece1e6cb22b77b2a8ad0212742d5
vasyna <vasyna@gmail.com>
2026-02-07 13:48:14 +0300

    Create dlinkdgs125052x.rb
    add support Dlink DGS-1250-52X Gigabit Ethernet Smart Managed Switch

---
+++ lib/oxidized/model/dlinkdgs125052x.rb
@@ -0,0 +1,32 @@
+class DlinkDgs125052x < Oxidized::Model
+  using Refinements
+
+  # D-LINK Switches
+
+  prompt /[\w.@()\/:-]+[#>]\s?$/
+  comment '# '
+
+  cmd :secret do |cfg|
+    cfg.gsub! /^(create snmp community) \S+/, '\\1 <removed>'
+    cfg.gsub! /^(create snmp group) \S+/, '\\1 <removed>'
+    cfg
+  end
+
+  cmd :all do |cfg|
+    cfg.each_line.to_a[2..-2].map { |line| line.delete("\r").rstrip }.join("\n") + "\n"
+  end
+
+  cmd 'show startup-config'
+
+  cfg :telnet do
+    username /\r*([\w\s.@()\/:-]+)?([Uu]ser[Nn]ame|[Ll]ogin):/
+    password /\r*[Pp]ass[Ww]ord:/
+  end
+
+  cfg :telnet, :ssh do
+    post_login 'disable clipaging'
+    post_login 'terminal length 0'
+    post_login 'terminal width 0'
+    pre_logout 'logout'
+  end
+end
