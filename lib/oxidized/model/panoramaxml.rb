231a1e9f44c2c5b90808f2f2cf4f3cab1ebbae0b
Adam Thompson [MERLIN] <athompson@merlin.mb.ca>
2026-05-05 15:50:40 -0500

    Create panoramaxml.rb

---
+++ lib/oxidized/model/panoramaxml.rb
@@ -0,0 +1,18 @@
+class PanoramaXML < Oxidized::Model
+
+  comment '! '
+
+  prompt /^[\w.@:()-]+[#>]\s?$/
+
+  cmd 'show' do |cfg|
+    cfg
+  end
+
+  cfg :ssh do
+    post_login 'set cli pager off'
+    post_login 'set cli config-output-format xml'
+    post_login 'configure'
+    pre_logout 'exit'
+    pre_logout 'quit'
+  end
+end
