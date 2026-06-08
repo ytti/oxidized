32e82d69c3e42fd21c85d084fad33b7a6482d01a
marbeg <martin.bergstrom@lm.se>
2026-06-08 09:23:18 +0200

    Workaround to use bash shell

--- lib/oxidized/model/tmos_aaa.rb
+++ lib/oxidized/model/tmos_aaa.rb
@@ -1,6 +1,10 @@
 class TMOS < Oxidized::Model
   using Refinements
 
+  # Use this model if you have AAA configured in tmos
+  # See https://github.com/ytti/oxidized/issues/437
+  # F5 KB: https://my.f5.com/manage/s/article/K28660000
+
   comment '# '
 
   cmd :secret do |cfg|
@@ -13,20 +17,20 @@ class TMOS < Oxidized::Model
     cfg
   end
 
-  cmd('tmsh -q show sys version') { |cfg| comment cfg }
+  cmd('run /util bash -c "tmsh -q show sys version"') { |cfg| comment cfg }
 
-  cmd('tmsh -q show sys software') { |cfg| comment cfg }
+  cmd('run /util bash -c "tmsh -q show sys software"') { |cfg| comment cfg }
 
-  cmd 'tmsh -q show sys hardware field-fmt' do |cfg|
+  cmd 'run /util bash -c "tmsh -q show sys hardware field-fmt"' do |cfg|
     cfg.gsub!(/fan-speed (\S+)/, '')
     cfg.gsub!(/temperature (\S+)/, '')
     cfg.gsub!(/humidity (\S+)/, '')
     comment cfg
   end
 
-  cmd('cat /config/bigip.license') { |cfg| comment cfg }
+  cmd('run /util bash -c "cat /config/bigip.license"') { |cfg| comment cfg }
 
-  cmd 'tmsh -q list' do |cfg|
+  cmd 'run /util bash -c "tmsh -q list"' do |cfg|
     cfg.gsub!(/state (up|down|checking|irule-down)/, '')
     cfg.gsub!(/errors (\d+)/, '')
     cfg.gsub!(/^\s+bandwidth-bps (\d+)/, '')
@@ -36,22 +40,22 @@ class TMOS < Oxidized::Model
     cfg
   end
 
-  cmd('tmsh -q list net route all') { |cfg| comment cfg }
+  cmd('run /util bash -c "tmsh -q list net route all"') { |cfg| comment cfg }
 
-  cmd('/bin/ls --full-time --color=never /config/ssl/ssl.crt') { |cfg| comment cfg }
+  cmd('run /util bash -c "/bin/ls --full-time --color=never /config/ssl/ssl.crt"') { |cfg| comment cfg }
 
-  cmd('/bin/ls --full-time --color=never /config/ssl/ssl.key') { |cfg| comment cfg }
+  cmd('run /util bash -c "/bin/ls --full-time --color=never /config/ssl/ssl.key"') { |cfg| comment cfg }
 
-  cmd 'tmsh -q show running-config sys db all-properties' do |cfg|
+  cmd 'run /util bash -c "tmsh -q show running-config sys db all-properties"' do |cfg|
     cfg.gsub!(/sys db configsync.localconfigtime {[^}]+}/m, '')
     cfg.gsub!(/sys db gtm.configtime {[^}]+}/m, '')
     cfg.gsub!(/sys db ltm.configtime {[^}]+}/m, '')
     comment cfg
   end
 
-  cmd('[ -d "/config/zebos" ] && cat /config/zebos/*/ZebOS.conf') { |cfg| comment cfg }
+  cmd('run /util bash -c "[ -d "/config/zebos" ] && cat /config/zebos/*/ZebOS.conf"') { |cfg| comment cfg }
 
-  cmd('cat /config/partitions/*/bigip*.conf') { |cfg| comment cfg }
+  cmd('run /util bash -c "cat /config/partitions/*/bigip*.conf"') { |cfg| comment cfg }
 
   cfg :ssh do
     exec true # don't run shell, run each command in exec channel
